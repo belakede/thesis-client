da5507c299e4d4ac237d99bc620699fdc72359d6
Adam Belak <belakede@gmail.com>
2016-11-07 16:41:44 +0100

    Load auth page on startup

--- src/main/java/me/belakede/thesis/client/boundary/javafx/controller/MainFrameController.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/controller/MainFrameController.java
@@ -1,18 +1,31 @@
 package me.belakede.thesis.client.boundary.javafx.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainFrameController implements Initializable {
 
     @FXML
-    private Pane parent;
+    private StackPane parent;
 
     public void initialize(URL location, ResourceBundle resources) {
+        try {
+            loadContent("auth");
+        } catch (IOException e) {
+            throw new RuntimeException("Can't load auth.fxml");
+        }
+    }
 
+    private void loadContent(String key) throws IOException {
+        Pane load = FXMLLoader.load(getClass().getResource("../" + key + ".fxml"));
+        parent.getChildren().clear();
+        parent.getChildren().add(load);
     }
 }
