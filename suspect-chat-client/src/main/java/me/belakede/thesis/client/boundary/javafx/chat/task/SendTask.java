f38ab90c6a7ac2490b11fef2f122b09edb4de95c
Adam Belak <belakede@gmail.com>
2016-11-07 11:41:18 +0100

    Add SendTask

---
+++ suspect-chat-client/src/main/java/me/belakede/thesis/client/boundary/javafx/chat/task/SendTask.java
@@ -0,0 +1,11 @@
+package me.belakede.thesis.client.boundary.javafx.chat.task;
+
+import javafx.concurrent.Task;
+import me.belakede.thesis.client.boundary.javafx.chat.model.Message;
+
+public class SendTask extends Task<Message> {
+    @Override
+    protected Message call() throws Exception {
+        return null;
+    }
+}
