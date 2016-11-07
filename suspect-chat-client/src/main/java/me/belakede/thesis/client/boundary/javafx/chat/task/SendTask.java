614a42af759d394233654601674abf6078aff3c1
Adam Belak <belakede@gmail.com>
2016-11-07 11:43:44 +0100

    Add message field and constructor to SendTask

--- suspect-chat-client/src/main/java/me/belakede/thesis/client/boundary/javafx/chat/task/SendTask.java
+++ suspect-chat-client/src/main/java/me/belakede/thesis/client/boundary/javafx/chat/task/SendTask.java
@@ -4,6 +4,13 @@ import javafx.concurrent.Task;
 import me.belakede.thesis.client.boundary.javafx.chat.model.Message;
 
 public class SendTask extends Task<Message> {
+
+    private final String message;
+
+    public SendTask(String message) {
+        this.message = message;
+    }
+
     @Override
     protected Message call() throws Exception {
         return null;
