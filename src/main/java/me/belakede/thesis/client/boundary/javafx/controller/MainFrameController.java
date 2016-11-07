4861a0b04db17da3c7f43c05ffe1686246252228
Adam Belak <belakede@gmail.com>
2016-11-07 16:02:19 +0100

    Add MainFrameController and main-frame.fxml

---
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/controller/MainFrameController.java
@@ -0,0 +1,12 @@
+package me.belakede.thesis.client.boundary.javafx.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainFrameController implements Initializable {
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
