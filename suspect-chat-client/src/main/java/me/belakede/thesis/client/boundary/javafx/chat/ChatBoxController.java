f161e5433e3738abe704aa3e2561d8ea9e29a0d0
Adam Belak <belakede@gmail.com>
2016-11-07 11:08:24 +0100

    Add simple chat-box window

---
+++ suspect-chat-client/src/main/java/me/belakede/thesis/client/boundary/javafx/chat/ChatBoxController.java
@@ -0,0 +1,16 @@
+package me.belakede.thesis.client.boundary.javafx.chat;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ChatBoxController implements Initializable {
+
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+    public void send(ActionEvent actionEvent) {
+    }
+}
