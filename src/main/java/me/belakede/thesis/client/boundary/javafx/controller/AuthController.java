16d6d51c27385bd58ed8e7bfe49f6e282f9183af
Adam Belak <belakede@gmail.com>
2016-11-07 16:57:53 +0100

    Change Auth view

--- src/main/java/me/belakede/thesis/client/boundary/javafx/controller/AuthController.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/controller/AuthController.java
@@ -1,12 +1,22 @@
 package me.belakede.thesis.client.boundary.javafx.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AuthController implements Initializable {
-    public void initialize(URL location, ResourceBundle resources) {
 
+    @FXML
+    private VBox parent;
+
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+    public void hide(ActionEvent actionEvent) {
+        parent.setVisible(false);
     }
 }
