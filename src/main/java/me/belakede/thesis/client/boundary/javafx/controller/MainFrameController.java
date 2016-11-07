e556c86264d6c1496e6c6276176d90e342e2c993
Adam Belak <belakede@gmail.com>
2016-11-07 16:33:36 +0100

    Change MainFrame

--- src/main/java/me/belakede/thesis/client/boundary/javafx/controller/MainFrameController.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/controller/MainFrameController.java
@@ -1,11 +1,17 @@
 package me.belakede.thesis.client.boundary.javafx.controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.Pane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainFrameController implements Initializable {
+
+    @FXML
+    private Pane parent;
+
     public void initialize(URL location, ResourceBundle resources) {
 
     }
