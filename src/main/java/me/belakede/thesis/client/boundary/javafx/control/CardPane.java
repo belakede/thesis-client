ef49abb3de2bca565fd611c3d436cc5ce8a7f480
Adam Belak <belakede@gmail.com>
2016-11-08 11:07:24 +0100

    Add Card model field

--- src/main/java/me/belakede/thesis/client/boundary/javafx/control/CardPane.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/control/CardPane.java
@@ -1,35 +1,51 @@
 package me.belakede.thesis.client.boundary.javafx.control;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
 import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
+import me.belakede.thesis.client.boundary.javafx.model.Card;
 import me.belakede.thesis.client.boundary.javafx.util.ControlLoader;
 
 public class CardPane extends StackPane {
 
+    private final ObjectProperty<Card> card = new SimpleObjectProperty<>();
     @FXML
     private Rectangle rectangle;
     @FXML
     private Text text;
 
     public CardPane() {
+        this(new Card());
+    }
+
+    public CardPane(Card card) {
         load();
+        setCard(card);
+        setBindings();
         rotate();
     }
 
-    public CardPane(me.belakede.thesis.game.equipment.Card card) {
-        load();
-        setLabel(card);
-        rotate();
+    public Card getCard() {
+        return card.get();
+    }
+
+    public void setCard(Card card) {
+        this.card.set(card);
+    }
+
+    public ObjectProperty<Card> cardProperty() {
+        return card;
     }
 
     private void load() {
         ControlLoader.load(this);
     }
 
-    private void setLabel(me.belakede.thesis.game.equipment.Card card) {
-        text.textProperty().setValue(card.name());
+    private void setBindings() {
+        text.textProperty().bind(cardProperty().get().cardProperty().asString());
     }
 
     private void rotate() {
