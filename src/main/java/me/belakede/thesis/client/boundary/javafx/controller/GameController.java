d4339f2ff7dc909e5403fe22d50a857d5247fbc1
Adam Belak <belakede@gmail.com>
2016-11-09 16:46:09 +0100

    Use RotateConfigurationPane

--- src/main/java/me/belakede/thesis/client/boundary/javafx/controller/GameController.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/controller/GameController.java
@@ -4,13 +4,10 @@ import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Side;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.transform.Rotate;
 import me.belakede.thesis.client.boundary.javafx.control.BoardPane;
 import me.belakede.thesis.client.boundary.javafx.control.CardPane;
+import me.belakede.thesis.client.boundary.javafx.control.RotateConfigurationPane;
 import me.belakede.thesis.client.boundary.javafx.model.Card;
 import me.belakede.thesis.game.equipment.BoardType;
 import me.belakede.thesis.game.equipment.Suspect;
@@ -30,26 +27,11 @@ public class GameController implements Initializable {
     private AnchorPane cardContainer;
     @FXML
     private HiddenSidesPane pane;
-    @FXML
-    private BorderPane hiddenPane;
-    @FXML
-    private Slider sliderHorizontal;
-    @FXML
-    private Slider sliderVertical;
-    @FXML
-    private Slider sliderTheThirdOne;
-    @FXML
-    private Label sliderHorizontalLabel;
-    @FXML
-    private Label sliderVerticalLabel;
-    @FXML
-    private Label sliderTheThirdOneLabel;
 
     public void initialize(URL location, ResourceBundle resources) {
         List<me.belakede.thesis.game.equipment.Card> cards = Arrays.asList(Suspect.MUSTARD, Suspect.PLUM, Weapon.CANDLESTICK, Weapon.KNIFE, Weapon.WRENCH);
         loadBoard(BoardType.DEFAULT);
         addCards(cards);
-        hookupChangeListeners();
     }
 
     private void loadBoard(BoardType boardType) {
@@ -71,29 +53,11 @@ public class GameController implements Initializable {
     }
 
     private void addRotation(BoardPane boardPane) {
-        Rotate horizontalRotate = new Rotate(0, 202.5, 202.5, 0, Rotate.X_AXIS);
-        Rotate verticalRotate = new Rotate(0, 202.5, 202.5, 0, Rotate.Y_AXIS);
-        Rotate theThirdOneRotate = new Rotate(0, 202.5, 202.5, 202.5, Rotate.Z_AXIS);
-        horizontalRotate.angleProperty().bind(sliderHorizontal.valueProperty());
-        verticalRotate.angleProperty().bind(sliderVertical.valueProperty());
-        theThirdOneRotate.angleProperty().bind(sliderTheThirdOne.valueProperty());
-        sliderHorizontalLabel.textProperty().bind(sliderHorizontal.valueProperty().asString("%.2f"));
-        sliderVerticalLabel.textProperty().bind(sliderVertical.valueProperty().asString("%.2f"));
-        sliderTheThirdOneLabel.textProperty().bind(sliderTheThirdOne.valueProperty().asString("%.2f"));
-        boardPane.getTransforms().addAll(horizontalRotate, verticalRotate, theThirdOneRotate);
+        RotateConfigurationPane configurationPane = new RotateConfigurationPane(boardPane.getMinWidth());
+        boardPane.getTransforms().addAll(configurationPane.getRotates());
+        pane.setRight(configurationPane);
+        pane.getRight().setOnMouseEntered(event -> pane.setPinnedSide(Side.RIGHT));
+        pane.getRight().setOnMouseExited(event -> pane.setPinnedSide(null));
     }
 
-    private void hookupChangeListeners() {
-        sliderHorizontal.valueProperty().addListener((observable, oldValue, newValue) -> {
-            sliderHorizontal.setValue(Math.round(newValue.doubleValue()));
-        });
-        sliderVertical.valueProperty().addListener((observable, oldValue, newValue) -> {
-            sliderVertical.setValue(Math.round(newValue.doubleValue()));
-        });
-        sliderTheThirdOne.valueProperty().addListener((observable, oldValue, newValue) -> {
-            sliderTheThirdOne.setValue(Math.round(newValue.doubleValue()));
-        });
-        hiddenPane.setOnMouseEntered(event -> pane.setPinnedSide(Side.RIGHT));
-        hiddenPane.setOnMouseExited(event -> pane.setPinnedSide(null));
-    }
 }
