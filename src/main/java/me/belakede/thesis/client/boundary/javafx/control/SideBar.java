28873016f3a62cbfb873f4616357bfcf3cf1d5b2
belakede <belakede@gmail.com>
2016-11-11 00:03:12 +0100

    Delegate getRotates() to SideBar

--- src/main/java/me/belakede/thesis/client/boundary/javafx/control/SideBar.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/control/SideBar.java
@@ -1,12 +1,22 @@
 package me.belakede.thesis.client.boundary.javafx.control;
 
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.scene.layout.VBox;
+import javafx.scene.transform.Rotate;
 import me.belakede.thesis.client.boundary.javafx.util.ControlLoader;
 
 public class SideBar extends VBox {
 
+    @FXML
+    private RotateConfigurationPane rotateConfiguration;
+
     public SideBar() {
         load();
+    }
+
+    public ObservableList<Rotate> getRotates() {
+        return rotateConfiguration.getRotates();
     }
 
     private void load() {
