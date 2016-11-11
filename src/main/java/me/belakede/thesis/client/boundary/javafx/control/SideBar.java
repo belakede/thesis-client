373659b1dfc9b9fdfb111c3341195276848aa129
Adam Belak <belakede@gmail.com>
2016-11-11 09:30:20 +0100

    Modify style class when figurine change

--- src/main/java/me/belakede/thesis/client/boundary/javafx/control/SideBar.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/control/SideBar.java
@@ -5,23 +5,32 @@ import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.transform.Rotate;
 import me.belakede.thesis.client.boundary.javafx.util.ControlLoader;
 import me.belakede.thesis.game.equipment.Figurine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SideBar extends VBox {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(SideBar.class);
+
     private final ObjectProperty<Figurine> figurine = new SimpleObjectProperty<>();
+    @FXML
+    private ImageView imageView;
     @FXML
     private RotateConfigurationPane rotateConfiguration;
 
     public SideBar() {
         load();
+        hookupChangeListeners();
     }
 
     public SideBar(Figurine figurine) {
         load();
+        hookupChangeListeners();
         setFigurine(figurine);
     }
 
@@ -48,5 +57,17 @@ public class SideBar extends VBox {
 
     private void load() {
         ControlLoader.load(this);
+    }
+
+    private void hookupChangeListeners() {
+        figurineProperty().addListener((observable, oldValue, newValue) -> {
+            LOGGER.info("Figurine changed from {} to {}", oldValue, newValue);
+            if (newValue != null) {
+                if (oldValue != null) {
+                    imageView.getStyleClass().remove(oldValue.name().toLowerCase());
+                }
+                imageView.getStyleClass().add(newValue.name().toLowerCase());
+            }
+        });
     }
 }
