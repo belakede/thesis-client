885ab93d050121c5113402feb14b55919209b75e
Adam Belak <belakede@gmail.com>
2016-11-11 10:52:04 +0100

    Clear CardPane

--- src/main/java/me/belakede/thesis/client/boundary/javafx/control/CardPane.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/control/CardPane.java
@@ -1,53 +1,16 @@
 package me.belakede.thesis.client.boundary.javafx.control;
 
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.fxml.FXML;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import me.belakede.thesis.client.boundary.javafx.model.Card;
+import javafx.scene.layout.HBox;
 import me.belakede.thesis.client.boundary.javafx.util.ControlLoader;
 
-public class CardPane extends StackPane {
+public class CardPane extends HBox {
 
-    private final ObjectProperty<Card> card = new SimpleObjectProperty<>();
-    @FXML
-    private Pane content;
-
-    public CardPane(Card card) {
+    public CardPane() {
         load();
-        setCard(card);
-        setStyleClass();
-        setOnMouseEnteredEvent();
-        rotate();
-    }
-
-    private void setStyleClass() {
-        content.getStyleClass().add(card.getValue().getCard().name().toLowerCase());
-    }
-
-    public Card getCard() {
-        return card.get();
-    }
-
-    public void setCard(Card card) {
-        this.card.set(card);
-    }
-
-    public ObjectProperty<Card> cardProperty() {
-        return card;
     }
 
     private void load() {
         ControlLoader.load(this);
-    }
-
-    private void setOnMouseEnteredEvent() {
-        setOnMouseEntered(event -> ((CardPane) event.getSource()).toFront());
-    }
-
-    private void rotate() {
-        content.rotateProperty().setValue(Math.random() * 40 - 20);
     }
 
 }
