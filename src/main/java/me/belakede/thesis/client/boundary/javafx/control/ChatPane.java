d8178ff0873c6ebdb0ed6ed21d1b0081da9c9c8a
Adam Belak <belakede@gmail.com>
2016-11-11 09:38:41 +0100

    Add figurine property field to ChatPane

--- src/main/java/me/belakede/thesis/client/boundary/javafx/control/ChatPane.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/control/ChatPane.java
@@ -1,13 +1,17 @@
 package me.belakede.thesis.client.boundary.javafx.control;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import me.belakede.thesis.client.boundary.javafx.util.ControlLoader;
+import me.belakede.thesis.game.equipment.Figurine;
 import org.controlsfx.control.PopOver;
 
 public class ChatPane extends StackPane {
 
+    private final ObjectProperty<Figurine> figurine = new SimpleObjectProperty<>();
     @FXML
     private Button chatButton;
     private PopOver popOver;
@@ -16,6 +20,25 @@ public class ChatPane extends StackPane {
         load();
         setupPopover();
         hookupChangeListeners();
+    }
+
+    public ChatPane(Figurine figurine) {
+        load();
+        setupPopover();
+        hookupChangeListeners();
+        setFigurine(figurine);
+    }
+
+    public Figurine getFigurine() {
+        return figurine.get();
+    }
+
+    public void setFigurine(Figurine figurine) {
+        this.figurine.set(figurine);
+    }
+
+    public ObjectProperty<Figurine> figurineProperty() {
+        return figurine;
     }
 
     private void load() {
@@ -31,6 +54,14 @@ public class ChatPane extends StackPane {
     }
 
     private void hookupChangeListeners() {
+        figurine.addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                if (oldValue != null) {
+                    chatButton.getStyleClass().remove(oldValue.name().toLowerCase());
+                }
+                chatButton.getStyleClass().add(newValue.name().toLowerCase());
+            }
+        });
         chatButton.setOnAction(event -> {
             if (popOver.isShowing()) {
                 popOver.hide();
