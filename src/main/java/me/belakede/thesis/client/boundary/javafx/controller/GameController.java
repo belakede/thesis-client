70cc4b8eb09e0670747d5d1e608327dd4f8e20d8
Adam Belak <belakede@gmail.com>
2016-11-11 13:11:44 +0100

    Set up cards

--- src/main/java/me/belakede/thesis/client/boundary/javafx/controller/GameController.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/controller/GameController.java
@@ -1,17 +1,19 @@
 package me.belakede.thesis.client.boundary.javafx.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import me.belakede.thesis.client.boundary.javafx.control.ChatPane;
 import me.belakede.thesis.client.boundary.javafx.control.GamePane;
 import me.belakede.thesis.client.boundary.javafx.control.NotePane;
 import me.belakede.thesis.game.Game;
-import me.belakede.thesis.game.equipment.BoardType;
-import me.belakede.thesis.game.equipment.Suspect;
+import me.belakede.thesis.game.equipment.*;
 import me.belakede.thesis.internal.game.util.GameBuilder;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
 public class GameController implements Initializable {
@@ -26,8 +28,10 @@ public class GameController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         try {
             Game game = GameBuilder.create().boardType(BoardType.DEFAULT).mystery().players(4).positions().build();
+            ObservableList<Card> cards = FXCollections.observableList(Arrays.asList(Weapon.CANDLESTICK, Room.DINING_ROOM, Weapon.LEAD_PIPE, Suspect.PEACOCK, Room.BILLIARD_ROOM));
             gamePane.setGame(game);
             gamePane.setFigurine(Suspect.PLUM);
+            gamePane.setCards(cards);
             notePane.setFigurine(Suspect.PLUM);
             chatPane.setFigurine(Suspect.PLUM);
         } catch (IOException e) {
