659cf6813da91baa1cb2843977e28ebe4fc6a54f
Adam Belak <belakede@gmail.com>
2016-11-11 17:36:43 +0100

    Add simple MarkerPane

---
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/control/MarkerPane.java
@@ -0,0 +1,12 @@
+package me.belakede.thesis.client.boundary.javafx.control;
+
+import javafx.scene.layout.HBox;
+
+import static me.belakede.thesis.client.boundary.javafx.util.ControlLoader.load;
+
+public class MarkerPane extends HBox {
+
+    public MarkerPane() {
+        load(this);
+    }
+}
