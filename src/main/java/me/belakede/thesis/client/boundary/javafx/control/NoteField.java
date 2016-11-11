61496fd8396e909ad391915faeeb460642be7b7b
Adam Belak <belakede@gmail.com>
2016-11-11 18:26:09 +0100

    Remove unnecessary fields, getters and setters

---
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/control/NoteField.java
@@ -0,0 +1,45 @@
+package me.belakede.thesis.client.boundary.javafx.control;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import me.belakede.thesis.client.boundary.javafx.util.ControlLoader;
+import me.belakede.thesis.game.equipment.Card;
+import me.belakede.thesis.game.equipment.Suspect;
+import org.controlsfx.control.PopOver;
+
+public class NoteField extends Pane {
+
+    @FXML
+    private Button markerButton;
+    private PopOver popOver;
+
+    public NoteField() {
+        load();
+        setupPopOver();
+        setupActionEvents();
+    }
+
+    public NoteField(String owner, Card card) {
+        load();
+        setupPopOver();
+        setupActionEvents();
+    }
+
+    private void load() {
+        ControlLoader.load(this);
+    }
+
+    private void setupPopOver() {
+        popOver = new PopOver(new MarkerPane("admin", Suspect.SCARLET));
+        popOver.setDetachable(false);
+        popOver.setDetached(false);
+        popOver.setAnimated(true);
+        popOver.setArrowLocation(PopOver.ArrowLocation.BOTTOM_CENTER);
+        popOver.setAutoHide(true);
+    }
+
+    private void setupActionEvents() {
+        markerButton.setOnAction(event -> popOver.show(this));
+    }
+}
