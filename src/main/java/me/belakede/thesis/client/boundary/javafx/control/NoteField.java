f0beafb50c1f07b556afacac0e4a2bb5fcd0e301
Adam Belak <belakede@gmail.com>
2016-11-11 18:38:48 +0100

    Use MarkerPane in NoteField

--- src/main/java/me/belakede/thesis/client/boundary/javafx/control/NoteField.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/control/NoteField.java
@@ -5,7 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import me.belakede.thesis.client.boundary.javafx.util.ControlLoader;
 import me.belakede.thesis.game.equipment.Card;
-import me.belakede.thesis.game.equipment.Suspect;
+import me.belakede.thesis.game.equipment.Marker;
 import org.controlsfx.control.PopOver;
 
 public class NoteField extends Pane {
@@ -13,9 +13,11 @@ public class NoteField extends Pane {
     @FXML
     private Button markerButton;
     private PopOver popOver;
+    private MarkerPane markerPane;
 
-    public NoteField(String owner, Card card) {
+    public NoteField(String owner, Card card, Marker marker) {
         load();
+        setupMarkerPane(owner, card, marker);
         setupPopOver();
         setupActionEvents();
     }
@@ -24,8 +26,12 @@ public class NoteField extends Pane {
         ControlLoader.load(this);
     }
 
+    private void setupMarkerPane(String owner, Card card, Marker marker) {
+        markerPane = new MarkerPane(owner, card, marker);
+    }
+
     private void setupPopOver() {
-        popOver = new PopOver(new MarkerPane("admin", Suspect.SCARLET));
+        popOver = new PopOver(markerPane);
         popOver.setDetachable(false);
         popOver.setDetached(false);
         popOver.setAnimated(true);
