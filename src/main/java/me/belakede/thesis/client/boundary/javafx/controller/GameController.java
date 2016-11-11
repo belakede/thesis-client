f39b95647b59e83b5571b8ad9a550379d05ab954
Adam Belak <belakede@gmail.com>
2016-11-11 09:34:06 +0100

    Load game and change figurine in GameController

--- src/main/java/me/belakede/thesis/client/boundary/javafx/controller/GameController.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/controller/GameController.java
@@ -1,13 +1,33 @@
 package me.belakede.thesis.client.boundary.javafx.controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import me.belakede.thesis.client.boundary.javafx.control.ChatPane;
+import me.belakede.thesis.client.boundary.javafx.control.GamePane;
+import me.belakede.thesis.game.Game;
+import me.belakede.thesis.game.equipment.BoardType;
+import me.belakede.thesis.game.equipment.Suspect;
+import me.belakede.thesis.internal.game.util.GameBuilder;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class GameController implements Initializable {
 
+    @FXML
+    private GamePane gamePane;
+    @FXML
+    private ChatPane chatPane;
+
     public void initialize(URL location, ResourceBundle resources) {
+        try {
+            Game game = GameBuilder.create().boardType(BoardType.DEFAULT).mystery().players(4).positions().build();
+            gamePane.setGame(game);
+            gamePane.setFigurine(Suspect.PEACOCK);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
