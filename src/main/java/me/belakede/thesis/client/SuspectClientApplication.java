f3fbc105f19a520a156f6b058e6812a25c163579
belakede <belakede@gmail.com>
2016-11-12 19:17:09 +0100

    Restore original application starter classes

--- src/main/java/me/belakede/thesis/client/SuspectClientApplication.java
+++ src/main/java/me/belakede/thesis/client/SuspectClientApplication.java
@@ -1,35 +1,13 @@
 package me.belakede.thesis.client;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Rectangle2D;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Screen;
-import javafx.stage.Stage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-public class SuspectClientApplication extends Application {
+@SpringBootApplication
+public class SuspectClientApplication {
 
     public static void main(String[] args) {
-        launch(args);
+        SpringApplication.run(JavaFXApplicationRunner.class, args);
     }
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("boundary/javafx/main-frame.fxml"));
-        Scene scene = new Scene(root, 1280, 800);
-
-        stage.setTitle("Test Application");
-        stage.setMinWidth(1280);
-        stage.setMinHeight(800);
-        stage.setResizable(false);
-        stage.setFullScreen(hasLowResolution());
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    private boolean hasLowResolution() {
-        Rectangle2D visualBounds = Screen.getPrimary().getVisualBounds();
-        return visualBounds.getWidth() <= 1280 || visualBounds.getHeight() <= 800;
-    }
 }
