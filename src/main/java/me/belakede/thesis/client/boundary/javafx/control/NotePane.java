1cf603f51b8008bb33da6dd80880938fda9da151
belakede <belakede@gmail.com>
2016-11-12 18:53:19 +0100

    Add notes and and bind notebox's properties

--- src/main/java/me/belakede/thesis/client/boundary/javafx/control/NotePane.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/control/NotePane.java
@@ -1,35 +1,30 @@
 package me.belakede.thesis.client.boundary.javafx.control;
 
-import javafx.beans.property.MapProperty;
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleMapProperty;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.collections.FXCollections;
+import javafx.beans.property.*;
+import javafx.collections.ObservableList;
 import javafx.collections.ObservableMap;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import me.belakede.thesis.client.boundary.javafx.model.Note;
 import me.belakede.thesis.client.boundary.javafx.util.ControlLoader;
-import me.belakede.thesis.game.equipment.*;
+import me.belakede.thesis.game.equipment.Figurine;
+import me.belakede.thesis.game.equipment.Suspect;
 import org.controlsfx.control.PopOver;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class NotePane extends StackPane {
 
     private final ObjectProperty<Figurine> figurine = new SimpleObjectProperty<>();
     private final MapProperty<Suspect, String> users = new SimpleMapProperty<>();
+    private final ListProperty<Note> notes = new SimpleListProperty<>();
     @FXML
     private Button noteButton;
     private PopOver popOver;
+    private NoteBox noteBox;
 
     public NotePane() {
         load();
+        setupNoteBox();
         setupPopover();
         hookupChangeListeners();
     }
@@ -58,30 +53,34 @@ public class NotePane extends StackPane {
         return users;
     }
 
+    public ObservableList<Note> getNotes() {
+        return notes.get();
+    }
+
+    public void setNotes(ObservableList<Note> notes) {
+        this.notes.set(notes);
+    }
+
+    public ListProperty<Note> notesProperty() {
+        return notes;
+    }
+
     private void load() {
         ControlLoader.load(this);
     }
 
     private void setupPopover() {
-        Map<Suspect, String> players = new HashMap<>();
-        players.put(Suspect.WHITE, "Player 1");
-        players.put(Suspect.GREEN, "Player 2");
-        players.put(Suspect.MUSTARD, "Player 3");
-        players.put(Suspect.PEACOCK, "Player 4");
-        players.put(Suspect.PLUM, "Player 5");
-        players.put(Suspect.SCARLET, "Player 6");
-        List<Note> notes = new ArrayList<>();
-        notes.add(new Note(Suspect.GREEN, "Player 3", Marker.YES));
-        notes.add(new Note(Weapon.CANDLESTICK, "Player 2", Marker.MAYBE));
-        notes.add(new Note(Room.DINING_ROOM, "Player 5", Marker.MAYBE_NOT));
-        notes.add(new Note(Suspect.MUSTARD, "Player 6", Marker.QUESTION));
-        notes.add(new Note(Weapon.KNIFE, "Player 1", Marker.NOT));
-        NoteBox noteBox = new NoteBox(FXCollections.observableMap(players), FXCollections.observableList(notes));
-        popOver = new PopOver(new VBox(noteBox));
+        popOver = new PopOver(noteBox);
         popOver.setAnimated(true);
         popOver.setTitle("Notes");
         popOver.setHeaderAlwaysVisible(true);
         popOver.setArrowLocation(PopOver.ArrowLocation.BOTTOM_LEFT);
+    }
+
+    private void setupNoteBox() {
+        noteBox = new NoteBox();
+        noteBox.playersProperty().bind(usersProperty());
+        noteBox.notesProperty().bind(notesProperty());
     }
 
     private void hookupChangeListeners() {
