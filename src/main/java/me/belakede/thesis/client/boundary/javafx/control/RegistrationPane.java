a5454049b169835fd66245550c1a7d580cb84d43
belakede <belakede@gmail.com>
2016-11-13 03:48:36 +0100

    Add RegistrationPane

---
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/control/RegistrationPane.java
@@ -0,0 +1,12 @@
+package me.belakede.thesis.client.boundary.javafx.control;
+
+import javafx.scene.layout.VBox;
+
+import static me.belakede.thesis.client.boundary.javafx.util.ControlLoader.load;
+
+public class RegistrationPane extends VBox {
+
+    public RegistrationPane() {
+        load(this);
+    }
+}
