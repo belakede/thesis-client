13a695f264559ca1767edd75a5ca7d0cfdc4520f
belakede <belakede@gmail.com>
2016-11-13 17:33:29 +0100

    Add NoteRegistrationTask

---
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/task/NoteRegistrationTask.java
@@ -0,0 +1,38 @@
+package me.belakede.thesis.client.boundary.javafx.task;
+
+import javafx.concurrent.Task;
+import me.belakede.thesis.client.configuration.UserConfiguration;
+import me.belakede.thesis.server.note.request.NoteRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+public class NoteRegistrationTask extends Task<Void> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(NoteRegistrationTask.class);
+
+    @Override
+    protected Void call() throws Exception {
+        UserConfiguration configuration = UserConfiguration.getInstance();
+        Client client = ClientBuilder.newClient();
+        WebTarget webTarget = client.target(configuration.getBaseUrl() + "/notes/join");
+        LOGGER.info("WebTarget: {}", webTarget);
+        Response response = webTarget.request().accept(MediaType.APPLICATION_JSON_TYPE)
+                .header("Authorization", "Bearer " + configuration.getToken().getAccessToken())
+                .post(Entity.json(new NoteRequest(configuration.getRoomId())));
+        if (response.getStatus() != 200) {
+            LOGGER.warn("HTTP error code : {}", response.getStatus());
+            LOGGER.warn("{}", response.toString());
+            throw new RuntimeException("Room registration failed!");
+        } else {
+            LOGGER.info("User was successfully registered!");
+        }
+        return null;
+    }
+}
