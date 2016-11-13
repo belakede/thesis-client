daebe98ed7b170f21cfa85526bb98b03f591c6af
belakede <belakede@gmail.com>
2016-11-13 17:35:19 +0100

    Remove createNoteList()

--- src/main/java/me/belakede/thesis/client/boundary/javafx/controller/GameController.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/controller/GameController.java
@@ -9,14 +9,16 @@ import me.belakede.thesis.client.boundary.javafx.control.ChatPane;
 import me.belakede.thesis.client.boundary.javafx.control.GamePane;
 import me.belakede.thesis.client.boundary.javafx.control.HistoryPane;
 import me.belakede.thesis.client.boundary.javafx.control.NotePane;
-import me.belakede.thesis.client.boundary.javafx.model.Note;
 import me.belakede.thesis.game.Game;
 import me.belakede.thesis.game.equipment.*;
 import me.belakede.thesis.internal.game.util.GameBuilder;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
 
 public class GameController implements Initializable {
 
@@ -34,15 +36,13 @@ public class GameController implements Initializable {
             Game game = GameBuilder.create().boardType(BoardType.DEFAULT).mystery().players(4).positions().build();
             ObservableList<Card> cards = FXCollections.observableList(Arrays.asList(Weapon.CANDLESTICK, Room.DINING_ROOM, Weapon.LEAD_PIPE, Suspect.PEACOCK, Room.BILLIARD_ROOM));
             ObservableMap<Suspect, String> players = FXCollections.observableMap(createSuspectPlayerMap());
-            ObservableList<Note> notes = FXCollections.observableList(createNoteList());
             gamePane.setGame(game);
             gamePane.setCards(cards);
-            gamePane.setFigurine(Suspect.PLUM);
-            notePane.setFigurine(Suspect.PLUM);
+            gamePane.setFigurine(Suspect.GREEN);
+            notePane.setFigurine(Suspect.GREEN);
             notePane.setUsers(players);
-            notePane.setNotes(notes);
-            historyPane.setFigurine(Suspect.PLUM);
-            chatPane.setFigurine(Suspect.PLUM);
+            historyPane.setFigurine(Suspect.GREEN);
+            chatPane.setFigurine(Suspect.GREEN);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -57,16 +57,6 @@ public class GameController implements Initializable {
         players.put(Suspect.PLUM, "Player 5");
         players.put(Suspect.SCARLET, "Player 6");
         return players;
-    }
-
-    private List<Note> createNoteList() {
-        List<Note> notes = new ArrayList<>();
-        notes.add(new Note(Suspect.MUSTARD, "Player 5", Marker.NOT));
-        notes.add(new Note(Suspect.PEACOCK, "Player 6", Marker.QUESTION));
-        notes.add(new Note(Weapon.CANDLESTICK, "Player 2", Marker.MAYBE_NOT));
-        notes.add(new Note(Weapon.CANDLESTICK, "Player 3", Marker.MAYBE));
-        notes.add(new Note(Room.DINING_ROOM, "Player 2", Marker.YES));
-        return notes;
     }
 
 }
