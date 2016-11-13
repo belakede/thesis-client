82f5dd50d66f9de1a31ffbe9956520d30286669a
belakede <belakede@gmail.com>
2016-11-13 13:07:35 +0100

    Add MessageSenderTask

---
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/task/MessageSenderTask.java
@@ -0,0 +1,42 @@
+package me.belakede.thesis.client.boundary.javafx.task;
+
+import javafx.concurrent.Task;
+import me.belakede.thesis.client.configuration.UserConfiguration;
+import me.belakede.thesis.server.chat.request.ChatRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+public class MessageSenderTask extends Task<Void> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageSenderTask.class);
+
+    private final String message;
+
+    public MessageSenderTask(String message) {
+        this.message = message;
+    }
+
+    @Override
+    protected Void call() throws Exception {
+        UserConfiguration configuration = UserConfiguration.getInstance();
+        Client client = ClientBuilder.newClient();
+        WebTarget webTarget = client.target(configuration.getBaseUrl() + "/chat/send");
+        LOGGER.debug("WebTarget: {}", webTarget);
+        Response response = webTarget.request().accept(MediaType.APPLICATION_JSON_TYPE)
+                .post(Entity.json(new ChatRequest(configuration.getRoomId(), message)));
+        if (response.getStatus() == 200) {
+            LOGGER.info("User was successfully created!");
+        } else {
+            LOGGER.warn("HTTP error code : {}", response.getStatus());
+            throw new RuntimeException("Authentication failed!");
+        }
+        return null;
+    }
+}
