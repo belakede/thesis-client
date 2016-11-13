f5700c76e42148070438ba59da1eae7d22e46a18
belakede <belakede@gmail.com>
2016-11-13 02:12:31 +0100

    Add ClientConfiguration

---
+++ src/main/java/me/belakede/thesis/client/configuration/ClientConfiguration.java
@@ -0,0 +1,8 @@
+package me.belakede.thesis.client.configuration;
+
+public class ClientConfiguration {
+
+    public static final String CLIENT_APP = "c48dfe30-f50e";
+    public static final String CLIENT_SECRET = "ef26cafe-557d-41d2-aec6-ff4a44c8871e";
+
+}
