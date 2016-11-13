b73e39cd0a78ce2afb419360b4dc212852476924
belakede <belakede@gmail.com>
2016-11-13 04:11:00 +0100

    Add getBasicAccessAuthenticationHeader()

--- src/main/java/me/belakede/thesis/client/configuration/ClientConfiguration.java
+++ src/main/java/me/belakede/thesis/client/configuration/ClientConfiguration.java
@@ -1,8 +1,14 @@
 package me.belakede.thesis.client.configuration;
 
+import java.util.Base64;
+
 public class ClientConfiguration {
 
     public static final String CLIENT_APP = "c48dfe30-f50e";
     public static final String CLIENT_SECRET = "ef26cafe-557d-41d2-aec6-ff4a44c8871e";
+
+    public static final String getBasicAccessAuthenticationHeader() {
+        return "Basic " + Base64.getEncoder().encodeToString((CLIENT_APP + ":" + CLIENT_SECRET).getBytes());
+    }
 
 }
