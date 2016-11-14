3f5f19612ba7646d07706a253131e05c359fb000
Adam Belak <belakede@gmail.com>
2016-11-14 15:11:18 +0100

    Add GameDetailsPane

---
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/control/GameDetailsPane.java
@@ -0,0 +1,104 @@
+package me.belakede.thesis.client.boundary.javafx.control;
+
+
+import javafx.beans.property.*;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import me.belakede.thesis.game.equipment.BoardType;
+
+import java.time.LocalDateTime;
+
+import static me.belakede.thesis.client.boundary.javafx.util.ControlLoader.load;
+
+public class GameDetailsPane extends BorderPane {
+
+    private final LongProperty gameId = new SimpleLongProperty();
+    private final ObjectProperty<LocalDateTime> created = new SimpleObjectProperty<>();
+    private final ObjectProperty<BoardType> boardType = new SimpleObjectProperty<>();
+    private final ListProperty<String> players = new SimpleListProperty<>();
+
+    @FXML
+    private Text idText;
+    @FXML
+    private Text createdText;
+    @FXML
+    private VBox boardBox;
+    @FXML
+    private FlowPane playersPane;
+
+    public GameDetailsPane(long gameId, LocalDateTime created, BoardType boardType, ObservableList<String> players) {
+        load(this);
+        setupBinginds();
+        hookupChangeListeners();
+        setGameId(gameId);
+        setCreated(created);
+        setBoardType(boardType);
+        setPlayers(players);
+    }
+
+    public long getGameId() {
+        return gameId.get();
+    }
+
+    public void setGameId(long gameId) {
+        this.gameId.set(gameId);
+    }
+
+    public LongProperty gameIdProperty() {
+        return gameId;
+    }
+
+    public LocalDateTime getCreated() {
+        return created.get();
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created.set(created);
+    }
+
+    public ObjectProperty<LocalDateTime> createdProperty() {
+        return created;
+    }
+
+    public BoardType getBoardType() {
+        return boardType.get();
+    }
+
+    public void setBoardType(BoardType boardType) {
+        this.boardType.set(boardType);
+    }
+
+    public ObjectProperty<BoardType> boardTypeProperty() {
+        return boardType;
+    }
+
+    public ObservableList<String> getPlayers() {
+        return players.get();
+    }
+
+    public void setPlayers(ObservableList<String> players) {
+        this.players.set(players);
+    }
+
+    public ListProperty<String> playersProperty() {
+        return players;
+    }
+
+    private void setupBinginds() {
+        idText.textProperty().bind(gameIdProperty().asString("#%d"));
+        createdText.textProperty().bind(createdProperty().asString());
+    }
+
+    private void hookupChangeListeners() {
+        boardTypeProperty().addListener((observable, oldValue, newValue) -> {
+            boardBox.getStyleClass().removeAll("default", "advanced");
+            boardBox.getStyleClass().add(newValue.name().toLowerCase());
+        });
+        playersProperty().addListener((observable, oldValue, newValue) -> newValue.stream().map(Text::new).forEach(player -> playersPane.getChildren().add(player)));
+    }
+
+}
