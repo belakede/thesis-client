c9b86b97bad41115176cda3cf00cc43cc710a79f
belakede <belakede@gmail.com>
2016-11-15 21:31:21 +0100

    Add sorryPane

--- src/main/java/me/belakede/thesis/client/boundary/javafx/controller/LoungeController.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/controller/LoungeController.java
@@ -1,11 +1,17 @@
 package me.belakede.thesis.client.boundary.javafx.controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.StackPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoungeController implements Initializable {
+
+    @FXML
+    private StackPane sorryPane;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
