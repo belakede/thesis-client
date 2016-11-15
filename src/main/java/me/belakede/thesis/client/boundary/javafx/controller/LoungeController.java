4a1d4c64f243627377aa1d349c17d4dfc291cb6d
belakede <belakede@gmail.com>
2016-11-15 21:07:58 +0100

    Add lounge

---
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/controller/LoungeController.java
@@ -0,0 +1,13 @@
+package me.belakede.thesis.client.boundary.javafx.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoungeController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
