26b1269bbf6a6989e264f3dac3ea726b1981d7a1
Adam Belak <belakede@gmail.com>
2016-11-16 14:40:20 +0100

    Refactor PlayersPane

--- src/main/java/me/belakede/thesis/client/boundary/javafx/control/PlayersPane.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/control/PlayersPane.java
@@ -1,53 +1,14 @@
 package me.belakede.thesis.client.boundary.javafx.control;
 
-import javafx.beans.property.ListProperty;
-import javafx.beans.property.SimpleListProperty;
-import javafx.collections.ObservableList;
-import javafx.concurrent.Task;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
 import javafx.scene.layout.VBox;
-import me.belakede.thesis.client.boundary.javafx.task.DownloadPlayersTask;
-
-import static me.belakede.thesis.client.boundary.javafx.util.ControlLoader.load;
+import me.belakede.thesis.client.service.SpringFxmlLoader;
 
 public class PlayersPane extends VBox {
 
-    private final ListProperty<String> players = new SimpleListProperty<>();
-
-    @FXML
-    private VBox parent;
-    @FXML
-    private ListView<String> playersView;
-    @FXML
-    private Button refreshButton;
+    private static final SpringFxmlLoader SPRING_FXML_LOADER = new SpringFxmlLoader();
 
     public PlayersPane() {
-        load(this);
-        setupActionEvents();
-        hookupChangeListeners();
-        downloadPlayers();
+        SPRING_FXML_LOADER.load(this);
     }
-
-    public ListProperty<String> playersProperty() {
-        return players;
-    }
-
-    private void setupActionEvents() {
-        refreshButton.setOnAction(event -> downloadPlayers());
-    }
-
-    private void hookupChangeListeners() {
-        playersView.itemsProperty().bind(players);
-    }
-
-    private void downloadPlayers() {
-        Task<ObservableList<String>> task = new DownloadPlayersTask();
-        task.setOnSucceeded(event -> players.setValue(task.getValue()));
-        Thread thread = new Thread(task);
-        thread.start();
-    }
-
 
 }
