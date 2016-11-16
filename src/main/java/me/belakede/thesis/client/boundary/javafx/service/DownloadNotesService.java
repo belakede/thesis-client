8f8b1bbd8b2f1159537fe48e34f5d28e065c37fd
belakede <belakede@gmail.com>
2016-11-16 23:09:23 +0100

    Use NoteService to store notes

--- src/main/java/me/belakede/thesis/client/boundary/javafx/service/DownloadNotesService.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/service/DownloadNotesService.java
@@ -5,6 +5,7 @@ import javafx.concurrent.Task;
 import me.belakede.thesis.client.boundary.javafx.model.Note;
 import me.belakede.thesis.client.boundary.javafx.task.NoteDownloaderTask;
 import me.belakede.thesis.client.service.GameService;
+import me.belakede.thesis.client.service.NoteService;
 import me.belakede.thesis.client.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,15 +15,22 @@ public class DownloadNotesService extends javafx.concurrent.Service<ObservableLi
 
     private final UserService userService;
     private final GameService gameService;
+    private final NoteService noteService;
 
     @Autowired
-    public DownloadNotesService(UserService userService, GameService gameService) {
+    public DownloadNotesService(UserService userService, GameService gameService, NoteService noteService) {
         this.userService = userService;
         this.gameService = gameService;
+        this.noteService = noteService;
+        setupService();
     }
 
     @Override
     protected Task<ObservableList<Note>> createTask() {
         return new NoteDownloaderTask(userService, gameService);
+    }
+
+    private void setupService() {
+        setOnSucceeded(event -> noteService.setNotes(getValue()));
     }
 }
