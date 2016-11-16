e10fd45472f94097e7cdb02361c524e4f4d3546b
belakede <belakede@gmail.com>
2016-11-16 21:10:13 +0100

    Change MessageSenderTask

--- src/main/java/me/belakede/thesis/client/boundary/javafx/task/MessageSenderTask.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/task/MessageSenderTask.java
@@ -1,7 +1,8 @@
 package me.belakede.thesis.client.boundary.javafx.task;
 
 import javafx.concurrent.Task;
-import me.belakede.thesis.client.configuration.UserConfiguration;
+import me.belakede.thesis.client.service.GameService;
+import me.belakede.thesis.client.service.UserService;
 import me.belakede.thesis.server.chat.request.ChatRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,21 +17,24 @@ public class MessageSenderTask extends Task<Void> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MessageSenderTask.class);
 
+    private final UserService userService;
+    private final GameService gameService;
     private final String message;
 
-    public MessageSenderTask(String message) {
+    public MessageSenderTask(UserService userService, GameService gameService, String message) {
+        this.userService = userService;
+        this.gameService = gameService;
         this.message = message;
     }
 
     @Override
     protected Void call() throws Exception {
-        UserConfiguration configuration = UserConfiguration.getInstance();
         Client client = ClientBuilder.newClient();
-        WebTarget webTarget = client.target(configuration.getBaseUrl() + "/chat/send");
+        WebTarget webTarget = client.target(userService.getUrl("/chat/send"));
         LOGGER.debug("WebTarget: {}", webTarget);
         Response response = webTarget.request()
-                .header("Authorization", "Bearer " + configuration.getToken().getAccessToken())
-                .post(Entity.json(new ChatRequest(configuration.getRoomId(), message)));
+                .header("Authorization", "Bearer " + userService.getAccessToken())
+                .post(Entity.json(new ChatRequest(gameService.getRoomId(), message)));
         if (response.getStatus() == 200) {
             LOGGER.info("User was successfully created!");
         } else {
