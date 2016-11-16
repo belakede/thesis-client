889327b514771077931dc977a8c281f821c0dcc5
Adam Belak <belakede@gmail.com>
2016-11-16 13:31:53 +0100

    Inject and use service classes instead of singleton classes

--- src/main/java/me/belakede/thesis/client/boundary/javafx/controller/LoungeController.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/controller/LoungeController.java
@@ -6,29 +6,40 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
-import me.belakede.thesis.client.configuration.GameConfiguration;
-import me.belakede.thesis.client.configuration.UserConfiguration;
+import me.belakede.thesis.client.service.GameService;
+import me.belakede.thesis.client.service.UserService;
 import me.belakede.thesis.game.equipment.Suspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
 import java.net.URL;
 import java.util.Map;
 import java.util.ResourceBundle;
 
+@Controller
 public class LoungeController implements Initializable {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LoungeController.class);
 
+    private final UserService userService;
+    private final GameService gameService;
     @FXML
     private StackPane sorryPane;
     @FXML
     private HBox playerContainer;
 
+    @Autowired
+    public LoungeController(UserService userService, GameService gameService) {
+        this.userService = userService;
+        this.gameService = gameService;
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        String username = UserConfiguration.getInstance().getUsername();
-        Map<Suspect, String> players = GameConfiguration.getInstance().getPlayers();
+        String username = userService.getUsername();
+        Map<Suspect, String> players = gameService.getPlayers();
         players.entrySet().forEach(entry -> {
             LOGGER.info("waiting for user: {}", entry.getValue());
             VBox playerBox = new VBox();
