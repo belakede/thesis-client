68299d7155fc18d8e09c5f5d61d59be3c1aacaa2
Adam Belak <belakede@gmail.com>
2016-11-16 15:05:20 +0100

    Create bean from CreateGameTask

--- src/main/java/me/belakede/thesis/client/boundary/javafx/task/CreateGameTask.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/task/CreateGameTask.java
@@ -1,16 +1,22 @@
 package me.belakede.thesis.client.boundary.javafx.task;
 
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import me.belakede.thesis.client.boundary.javafx.model.GameSummary;
-import me.belakede.thesis.client.configuration.UserConfiguration;
+import me.belakede.thesis.client.service.UserService;
 import me.belakede.thesis.game.equipment.BoardType;
 import me.belakede.thesis.jackson.JacksonContextResolver;
 import me.belakede.thesis.server.game.request.GamesRequest;
 import me.belakede.thesis.server.game.response.GamesResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -18,27 +24,52 @@ import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 
+@Component
 public class CreateGameTask extends Task<GameSummary> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CreateGameTask.class);
 
-    private final BoardType boardType;
-    private final ObservableList<String> players;
+    private final ObjectProperty<BoardType> boardType = new SimpleObjectProperty<>();
+    private final ListProperty<String> players = new SimpleListProperty<>();
+    private final UserService userService;
 
-    public CreateGameTask(BoardType boardType, ObservableList<String> players) {
-        this.boardType = boardType;
-        this.players = players;
+    @Autowired
+    public CreateGameTask(UserService userService) {
+        this.userService = userService;
+    }
+
+    public BoardType getBoardType() {
+        return boardType.get();
+    }
+
+    public void setBoardType(BoardType boardType) {
+        this.boardType.set(boardType);
+    }
+
+    public ObjectProperty<BoardType> boardTypeProperty() {
+        return boardType;
+    }
+
+    public ObservableList<String> getPlayers() {
+        return players.get();
+    }
+
+    public void setPlayers(ObservableList<String> players) {
+        this.players.set(players);
+    }
+
+    public ListProperty<String> playersProperty() {
+        return players;
     }
 
     @Override
     protected GameSummary call() throws Exception {
-        UserConfiguration configuration = UserConfiguration.getInstance();
         Client client = ClientBuilder.newBuilder().register(JacksonContextResolver.class).build();
-        WebTarget webTarget = client.target(configuration.getBaseUrl() + "/games");
+        WebTarget webTarget = client.target(userService.getUrl("/games"));
         LOGGER.debug("WebTarget: {}", webTarget);
         GamesResponse response = webTarget.request().accept(MediaType.APPLICATION_JSON_TYPE)
-                .header("Authorization", "Bearer " + configuration.getToken().getAccessToken())
-                .post(Entity.json(new GamesRequest(boardType, players)), GamesResponse.class);
+                .header("Authorization", "Bearer " + userService.getAccessToken())
+                .post(Entity.json(new GamesRequest(getBoardType(), getPlayers())), GamesResponse.class);
         LOGGER.info("Game has been successful created!");
         return new GameSummary(response.getId(), response.getRoomId(), response.getTime(), response.getBoardType(), response.getStatus(), FXCollections.observableMap(response.getUsers()));
     }
