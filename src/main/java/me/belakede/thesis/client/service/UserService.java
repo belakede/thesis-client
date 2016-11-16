9b660d1cd5587fb046213b83ae0eb116f9b9a59f
Adam Belak <belakede@gmail.com>
2016-11-16 13:21:04 +0100

    Add UserService

---
+++ src/main/java/me/belakede/thesis/client/service/UserService.java
@@ -0,0 +1,65 @@
+package me.belakede.thesis.client.service;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import org.apache.el.parser.Token;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final StringProperty username = new SimpleStringProperty();
+    private final StringProperty baseUrl = new SimpleStringProperty();
+    private final StringProperty roomId = new SimpleStringProperty();
+    private final ObjectProperty<Token> token = new SimpleObjectProperty<>();
+
+    public String getUsername() {
+        return username.get();
+    }
+
+    public void setUsername(String username) {
+        this.username.set(username);
+    }
+
+    public StringProperty usernameProperty() {
+        return username;
+    }
+
+    public String getBaseUrl() {
+        return baseUrl.get();
+    }
+
+    public void setBaseUrl(String baseUrl) {
+        this.baseUrl.set(baseUrl);
+    }
+
+    public StringProperty baseUrlProperty() {
+        return baseUrl;
+    }
+
+    public String getRoomId() {
+        return roomId.get();
+    }
+
+    public void setRoomId(String roomId) {
+        this.roomId.set(roomId);
+    }
+
+    public StringProperty roomIdProperty() {
+        return roomId;
+    }
+
+    public Token getToken() {
+        return token.get();
+    }
+
+    public void setToken(Token token) {
+        this.token.set(token);
+    }
+
+    public ObjectProperty<Token> tokenProperty() {
+        return token;
+    }
+}
