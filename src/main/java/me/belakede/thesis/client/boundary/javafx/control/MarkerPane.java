0701110079060dba1d611e4e6eadd30ae0124f5e
belakede <belakede@gmail.com>
2016-11-16 23:45:17 +0100

    Add and bind property to access icon

--- src/main/java/me/belakede/thesis/client/boundary/javafx/control/MarkerPane.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/control/MarkerPane.java
@@ -1,5 +1,7 @@
 package me.belakede.thesis.client.boundary.javafx.control;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.layout.HBox;
 import me.belakede.thesis.client.boundary.javafx.control.controller.MarkerPaneController;
 import me.belakede.thesis.client.service.SpringFxmlLoader;
@@ -9,12 +11,25 @@ import me.belakede.thesis.game.equipment.Marker;
 public class MarkerPane extends HBox {
 
     private static final SpringFxmlLoader SPRING_FXML_LOADER = new SpringFxmlLoader();
+    private final ObjectProperty<Object> icon = new SimpleObjectProperty<>();
 
     public MarkerPane(String owner, Card card, Marker marker) {
         MarkerPaneController controller = SPRING_FXML_LOADER.load(this);
+        icon.bind(controller.iconProperty());
         controller.setCard(card);
         controller.setOwner(owner);
         controller.setMarker(marker);
     }
 
+    public Object getIcon() {
+        return icon.get();
+    }
+
+    public void setIcon(Object icon) {
+        this.icon.set(icon);
+    }
+
+    public ObjectProperty<Object> iconProperty() {
+        return icon;
+    }
 }
