4710022c42076d0e915c2e1f119c581ae7cff1c8
belakede <belakede@gmail.com>
2016-11-16 23:53:35 +0100

    Change NoteField

--- src/main/java/me/belakede/thesis/client/boundary/javafx/control/NoteField.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/control/NoteField.java
@@ -1,54 +1,18 @@
 package me.belakede.thesis.client.boundary.javafx.control;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
-import me.belakede.thesis.client.boundary.javafx.util.ControlLoader;
+import me.belakede.thesis.client.boundary.javafx.control.controller.NoteFieldController;
+import me.belakede.thesis.client.service.SpringFxmlLoader;
 import me.belakede.thesis.game.equipment.Card;
 import me.belakede.thesis.game.equipment.Marker;
-import org.controlsfx.control.PopOver;
-import org.controlsfx.glyphfont.Glyph;
 
 public class NoteField extends Pane {
 
-    @FXML
-    private Button markerButton;
-    @FXML
-    private Glyph markerGlyph;
-    private PopOver popOver;
-    private MarkerPane markerPane;
+    private static final SpringFxmlLoader SPRING_FXML_LOADER = new SpringFxmlLoader();
 
     public NoteField(String owner, Card card, Marker marker) {
-        load();
-        setupMarkerPane(owner, card, marker);
-        setupPopOver();
-        setupActionEvents();
-        hookupChangeListeners();
+        NoteFieldController controller = SPRING_FXML_LOADER.load(this);
+        controller.setMarkerPane(new MarkerPane(owner, card, marker));
     }
 
-    private void load() {
-        ControlLoader.load(this);
-    }
-
-    private void setupMarkerPane(String owner, Card card, Marker marker) {
-        markerPane = new MarkerPane(owner, card, marker);
-    }
-
-    private void setupPopOver() {
-        popOver = new PopOver(markerPane);
-        popOver.setDetachable(false);
-        popOver.setDetached(false);
-        popOver.setAnimated(true);
-        popOver.setArrowLocation(PopOver.ArrowLocation.BOTTOM_CENTER);
-        popOver.setAutoHide(true);
-    }
-
-    private void setupActionEvents() {
-        markerButton.setOnAction(event -> popOver.show(this));
-    }
-
-    private void hookupChangeListeners() {
-        markerGlyph.iconProperty().bind(markerPane.iconProperty());
-        markerGlyph.iconProperty().addListener((observable, oldValue, newValue) -> popOver.hide());
-    }
 }
