18362b77bad01d8a1552d674c1417059ee282e48
belakede <belakede@gmail.com>
2016-11-16 20:56:33 +0100

    Change NoteRegistrationTask

--- src/main/java/me/belakede/thesis/client/boundary/javafx/task/NoteRegistrationTask.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/task/NoteRegistrationTask.java
@@ -1,10 +1,13 @@
 package me.belakede.thesis.client.boundary.javafx.task;
 
 import javafx.concurrent.Task;
-import me.belakede.thesis.client.configuration.UserConfiguration;
+import me.belakede.thesis.client.service.GameService;
+import me.belakede.thesis.client.service.UserService;
 import me.belakede.thesis.server.note.request.NoteRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -13,19 +16,28 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+@Service
 public class NoteRegistrationTask extends Task<Void> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(NoteRegistrationTask.class);
 
+    private final UserService userService;
+    private final GameService gameService;
+
+    @Autowired
+    public NoteRegistrationTask(UserService userService, GameService gameService) {
+        this.userService = userService;
+        this.gameService = gameService;
+    }
+
     @Override
     protected Void call() throws Exception {
-        UserConfiguration configuration = UserConfiguration.getInstance();
         Client client = ClientBuilder.newClient();
-        WebTarget webTarget = client.target(configuration.getBaseUrl() + "/notes/join");
+        WebTarget webTarget = client.target(userService.getUrl("/notes/join"));
         LOGGER.info("WebTarget: {}", webTarget);
         Response response = webTarget.request().accept(MediaType.APPLICATION_JSON_TYPE)
-                .header("Authorization", "Bearer " + configuration.getToken().getAccessToken())
-                .post(Entity.json(new NoteRequest(configuration.getRoomId())));
+                .header("Authorization", "Bearer " + userService.getAccessToken())
+                .post(Entity.json(new NoteRequest(gameService.getRoomId())));
         if (response.getStatus() != 200) {
             LOGGER.warn("HTTP error code : {}", response.getStatus());
             LOGGER.warn("{}", response.toString());
