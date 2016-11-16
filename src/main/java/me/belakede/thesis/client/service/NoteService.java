a8a03a83173fb4a0fcd020c4c26b7144bdc1b1ff
belakede <belakede@gmail.com>
2016-11-16 22:42:25 +0100

    Remove CardsOrder from NoteService

--- src/main/java/me/belakede/thesis/client/service/NoteService.java
+++ src/main/java/me/belakede/thesis/client/service/NoteService.java
@@ -1,20 +1,15 @@
 package me.belakede.thesis.client.service;
 
 import javafx.beans.property.ListProperty;
-import javafx.beans.property.MapProperty;
 import javafx.beans.property.SimpleListProperty;
-import javafx.beans.property.SimpleMapProperty;
 import javafx.collections.ObservableList;
-import javafx.collections.ObservableMap;
 import me.belakede.thesis.client.boundary.javafx.model.Note;
-import me.belakede.thesis.game.equipment.Card;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NoteService {
 
     private final ListProperty<Note> notes = new SimpleListProperty<>();
-    private final MapProperty<Card, Integer> cardsOrder = new SimpleMapProperty<>();
 
     public ObservableList<Note> getNotes() {
         return notes.get();
@@ -28,15 +23,4 @@ public class NoteService {
         return notes;
     }
 
-    public ObservableMap<Card, Integer> getCardsOrder() {
-        return cardsOrder.get();
-    }
-
-    public void setCardsOrder(ObservableMap<Card, Integer> cardsOrder) {
-        this.cardsOrder.set(cardsOrder);
-    }
-
-    public MapProperty<Card, Integer> cardsOrderProperty() {
-        return cardsOrder;
-    }
 }
