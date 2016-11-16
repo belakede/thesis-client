88ab48ad74d535b298155b2db45160a651cd4dae
belakede <belakede@gmail.com>
2016-11-16 21:16:59 +0100

    Change MessageReceiverTask

--- src/main/java/me/belakede/thesis/client/boundary/javafx/task/MessageReceiverTask.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/task/MessageReceiverTask.java
@@ -1,10 +1,10 @@
 package me.belakede.thesis.client.boundary.javafx.task;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import me.belakede.thesis.client.boundary.javafx.model.UserMessage;
-import me.belakede.thesis.client.configuration.UserConfiguration;
+import me.belakede.thesis.client.service.GameService;
+import me.belakede.thesis.client.service.MessageService;
+import me.belakede.thesis.client.service.UserService;
 import me.belakede.thesis.game.equipment.Suspect;
 import me.belakede.thesis.jackson.JacksonContextResolver;
 import me.belakede.thesis.server.chat.request.ChatRequest;
@@ -14,49 +14,50 @@ import org.glassfish.jersey.media.sse.InboundEvent;
 import org.glassfish.jersey.media.sse.SseFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
-import java.util.List;
 
-public class MessageReceiverTask extends Task<List<UserMessage>> {
+@Service
+public class MessageReceiverTask extends Task<Void> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MessageReceiverTask.class);
-    private ObservableList<UserMessage> userMessages = FXCollections.observableArrayList();
 
-    public MessageReceiverTask() {
-    }
+    private final UserService userService;
+    private final GameService gameService;
+    private final MessageService messageService;
 
-    public ObservableList<UserMessage> getUserMessages() {
-        return userMessages;
-    }
-
-    public void setUserMessages(ObservableList<UserMessage> userMessages) {
-        this.userMessages = userMessages;
+    @Autowired
+    public MessageReceiverTask(UserService userService, GameService gameService, MessageService messageService) {
+        this.userService = userService;
+        this.gameService = gameService;
+        this.messageService = messageService;
     }
 
     @Override
-    protected List<UserMessage> call() throws Exception {
-        UserConfiguration configuration = UserConfiguration.getInstance();
+    protected Void call() throws Exception {
         Client client = ClientBuilder.newBuilder().register(JacksonContextResolver.class, SseFeature.class).build();
-        WebTarget webTarget = client.target(configuration.getBaseUrl() + "/chat/join");
+        WebTarget webTarget = client.target(userService.getUrl("/chat/join"));
         LOGGER.debug("WebTarget: {}", webTarget);
         EventInput eventInput = webTarget.request().accept(MediaType.APPLICATION_JSON_TYPE)
-                .header("Authorization", "Bearer " + configuration.getToken().getAccessToken())
-                .post(Entity.json(new ChatRequest(configuration.getRoomId())), EventInput.class);
+                .header("Authorization", "Bearer " + userService.getAccessToken())
+                .post(Entity.json(new ChatRequest(gameService.getRoomId())), EventInput.class);
         while (!eventInput.isClosed()) {
             final InboundEvent inboundEvent = eventInput.read();
             if (inboundEvent == null) {
+                LOGGER.warn("Connection lost! InboundEvent is null!");
                 break;
             }
             LOGGER.info("UserMessage arrived: {}", inboundEvent.toString());
             Message message = inboundEvent.readData(Message.class, MediaType.APPLICATION_JSON_TYPE);
-            userMessages.add(new UserMessage(message.getSender(), message.getContent(), message.getTime(), Suspect.PEACOCK));
+            messageService.add(new UserMessage(message.getSender(), message.getContent(), message.getTime(), Suspect.PEACOCK));
         }
         LOGGER.info("Channel closed!");
-        return userMessages;
+        return null;
     }
 }
