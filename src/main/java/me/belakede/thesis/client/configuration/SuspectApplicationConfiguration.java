c7b7d2e3d82b0db6eeae44a0ee5cd2ecc2f51fca
Adam Belak <belakede@gmail.com>
2016-11-16 13:07:39 +0100

    Add SuspectApplicationConfiguration

---
+++ src/main/java/me/belakede/thesis/client/configuration/SuspectApplicationConfiguration.java
@@ -0,0 +1,9 @@
+package me.belakede.thesis.client.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("me.belakede.thesis.client")
+public class SuspectApplicationConfiguration {
+}
