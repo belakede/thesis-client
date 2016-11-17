51986f8c87cdacc02fb57b58a2f6a047a4c1c4ad
belakede <belakede@gmail.com>
2016-11-18 00:04:45 +0100

    Change SideBar

--- src/main/java/me/belakede/thesis/client/boundary/javafx/control/SideBar.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/control/SideBar.java
@@ -1,14 +1,28 @@
 package me.belakede.thesis.client.boundary.javafx.control;
 
+import javafx.beans.property.ReadOnlyDoubleProperty;
+import javafx.collections.ObservableList;
 import javafx.scene.layout.VBox;
+import javafx.scene.transform.Rotate;
+import me.belakede.thesis.client.boundary.javafx.control.controller.SideBarController;
 import me.belakede.thesis.client.service.SpringFxmlLoader;
 
 public class SideBar extends VBox {
 
     private static final SpringFxmlLoader SPRING_FXML_LOADER = new SpringFxmlLoader();
+    private final SideBarController controller;
 
     public SideBar() {
-        SPRING_FXML_LOADER.load(this);
+        controller = SPRING_FXML_LOADER.load(this);
+    }
+
+    public void bindSize(ReadOnlyDoubleProperty value) {
+        controller.getRotateConfiguration().sizeProperty().unbind();
+        controller.getRotateConfiguration().sizeProperty().bind(value);
+    }
+
+    public ObservableList<Rotate> getRotates() {
+        return controller.getRotateConfiguration().getRotates();
     }
 
 }
