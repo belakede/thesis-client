b44b07fb99cece24b4d37fabdd00c75887a2db48
Adam Belak <belakede@gmail.com>
2016-11-17 16:02:18 +0100

    Change HistoryBox

--- src/main/java/me/belakede/thesis/client/boundary/javafx/control/HistoryBox.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/control/HistoryBox.java
@@ -1,43 +1,14 @@
 package me.belakede.thesis.client.boundary.javafx.control;
 
-import javafx.beans.property.ListProperty;
-import javafx.beans.property.SimpleListProperty;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
 import javafx.scene.layout.VBox;
-import me.belakede.thesis.client.boundary.javafx.util.ControlLoader;
+import me.belakede.thesis.client.service.SpringFxmlLoader;
 
 public class HistoryBox extends VBox {
 
-    private ListProperty<String> events = new SimpleListProperty<>();
-    @FXML
-    private ListView<String> eventList;
+    private static final SpringFxmlLoader SPRING_FXML_LOADER = new SpringFxmlLoader();
 
     public HistoryBox() {
-        load();
-        //bindEvents();
+        SPRING_FXML_LOADER.load(this);
     }
-
-    public ObservableList<String> getEvents() {
-        return events.get();
-    }
-
-    public void setEvents(ObservableList<String> events) {
-        this.events.set(events);
-    }
-
-    public ListProperty<String> eventsProperty() {
-        return events;
-    }
-
-    private void load() {
-        ControlLoader.load(this);
-    }
-
-    private void bindEvents() {
-        eventList.itemsProperty().bind(events);
-    }
-
 
 }
