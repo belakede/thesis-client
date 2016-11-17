22858021e3ea77e4745e5e70a17b7630c2808058
belakede <belakede@gmail.com>
2016-11-17 23:36:36 +0100

    Add BoardPaneController

---
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/control/controller/BoardPaneController.java
@@ -0,0 +1,80 @@
+package me.belakede.thesis.client.boundary.javafx.control.controller;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.TilePane;
+import me.belakede.thesis.client.boundary.javafx.control.FieldPane;
+import me.belakede.thesis.client.service.GameService;
+import me.belakede.thesis.client.service.UserService;
+import me.belakede.thesis.game.equipment.BoardType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Controller
+public class BoardPaneController implements Initializable {
+
+    private final ObjectProperty<BoardType> boardType = new SimpleObjectProperty<>();
+
+    private final UserService userService;
+    private final GameService gameService;
+
+    @FXML
+    private TilePane parent;
+
+    @Autowired
+    public BoardPaneController(UserService userService, GameService gameService) {
+        this.userService = userService;
+        this.gameService = gameService;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        hookupChangeListeners();
+        setupBindings();
+    }
+
+    private void hookupChangeListeners() {
+        boardType.addListener((observable, oldValue, newValue) -> {
+            if (null != newValue) {
+                setFields();
+                setDimension();
+                setStyleClass(oldValue, newValue);
+            }
+        });
+    }
+
+    private void setupBindings() {
+        boardType.bind(gameService.boardTypeProperty());
+    }
+
+    private void setFields() {
+        int size = gameService.getBoardType().getSize();
+        IntStream.range(0, size)
+                .mapToObj(i -> IntStream.range(0, size)
+                        .mapToObj(j -> new FieldPane(i, j))
+                        .collect(Collectors.toList()))
+                .forEach(fieldPanes -> parent.getChildren().addAll(fieldPanes));
+    }
+
+    private void setDimension() {
+        int newSize = gameService.getBoardType().getSize() * 25;
+        parent.setMinSize(newSize, newSize);
+        parent.setMaxSize(newSize, newSize);
+        parent.setPrefSize(newSize, newSize);
+    }
+
+    private void setStyleClass(BoardType oldValue, BoardType newValue) {
+        if (oldValue != null) {
+            parent.getStyleClass().remove(oldValue.name().toLowerCase());
+        }
+        parent.getStyleClass().add(newValue.name().toLowerCase());
+    }
+
+}
