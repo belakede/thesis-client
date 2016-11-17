1b901b3fe468cf47209121374d167f09b726eb3a
Adam Belak <belakede@gmail.com>
2016-11-17 13:36:52 +0100

    Add argument to constructor to execute when status is IN_PROGRESS

--- src/main/java/me/belakede/thesis/client/boundary/javafx/control/GameDetailsPane.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/control/GameDetailsPane.java
@@ -4,12 +4,13 @@ import javafx.scene.layout.BorderPane;
 import me.belakede.thesis.client.boundary.javafx.control.controller.GameDetailsPaneController;
 import me.belakede.thesis.client.boundary.javafx.model.GameSummary;
 import me.belakede.thesis.client.service.SpringFxmlLoader;
+import me.belakede.thesis.server.game.domain.Status;
 
 public class GameDetailsPane extends BorderPane {
 
     private static final SpringFxmlLoader SPRING_FXML_LOADER = new SpringFxmlLoader();
 
-    public GameDetailsPane(GameSummary gameSummary, Runnable remove) {
+    public GameDetailsPane(GameSummary gameSummary, Runnable remove, Runnable running) {
         GameDetailsPaneController controller = SPRING_FXML_LOADER.load(this);
         controller.gameIdProperty().bindBidirectional(gameSummary.idProperty());
         controller.boardTypeProperty().bindBidirectional(gameSummary.boardTypeProperty());
@@ -19,6 +20,11 @@ public class GameDetailsPane extends BorderPane {
         controller.removedProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue) {
                 remove.run();
+            }
+        });
+        controller.statusProperty().addListener((observable, oldValue, newValue) -> {
+            if (Status.IN_PROGRESS.equals(newValue)) {
+                running.run();
             }
         });
     }
