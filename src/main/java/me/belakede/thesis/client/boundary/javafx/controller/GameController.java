fb803a7cf2c9cb68106caba64be1663d31f32534
Adam Belak <belakede@gmail.com>
2016-11-17 16:33:04 +0100

    Fix GameController

--- src/main/java/me/belakede/thesis/client/boundary/javafx/controller/GameController.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/controller/GameController.java
@@ -9,20 +9,22 @@ import me.belakede.thesis.client.boundary.javafx.control.ChatPane;
 import me.belakede.thesis.client.boundary.javafx.control.GamePane;
 import me.belakede.thesis.client.boundary.javafx.control.HistoryPane;
 import me.belakede.thesis.client.boundary.javafx.control.NotePane;
+import me.belakede.thesis.client.service.NotificationService;
 import me.belakede.thesis.game.Game;
 import me.belakede.thesis.game.equipment.*;
 import me.belakede.thesis.internal.game.util.GameBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.ResourceBundle;
 
 @Controller
 public class GameController implements Initializable {
+
+    private final NotificationService notificationService;
 
     @FXML
     private GamePane gamePane;
@@ -33,30 +35,22 @@ public class GameController implements Initializable {
     @FXML
     private ChatPane chatPane;
 
+    @Autowired
+    public GameController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
     public void initialize(URL location, ResourceBundle resources) {
         try {
             Game game = GameBuilder.create().boardType(BoardType.DEFAULT).mystery().players(4).positions().build();
             ObservableList<Card> cards = FXCollections.observableList(Arrays.asList(Weapon.CANDLESTICK, Room.DINING_ROOM, Weapon.LEAD_PIPE, Suspect.PEACOCK, Room.BILLIARD_ROOM));
-            ObservableMap<Suspect, String> players = FXCollections.observableMap(createSuspectPlayerMap());
+            ObservableMap<Suspect, String> players = FXCollections.observableMap(notificationService.getGameStatusNotification().getPlayers());
             gamePane.setGame(game);
             gamePane.setCards(cards);
             gamePane.setFigurine(Suspect.GREEN);
-            historyPane.setFigurine(Suspect.GREEN);
-            chatPane.setFigurine(Suspect.GREEN);
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    private Map<Suspect, String> createSuspectPlayerMap() {
-        Map<Suspect, String> players = new HashMap<>();
-        players.put(Suspect.WHITE, "Player 1");
-        players.put(Suspect.GREEN, "Player 2");
-        players.put(Suspect.MUSTARD, "Player 3");
-        players.put(Suspect.PEACOCK, "Player 4");
-        players.put(Suspect.PLUM, "Player 5");
-        players.put(Suspect.SCARLET, "Player 6");
-        return players;
     }
 
 }
