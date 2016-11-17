5f100cb0eb43614c10fecd8ea7ed1513e89e26ee
belakede <belakede@gmail.com>
2016-11-17 23:58:01 +0100

    Change GamePane

--- src/main/java/me/belakede/thesis/client/boundary/javafx/control/GamePane.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/control/GamePane.java
@@ -1,104 +1,14 @@
 package me.belakede.thesis.client.boundary.javafx.control;
 
-import javafx.beans.property.ListProperty;
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleListProperty;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
 import javafx.scene.layout.BorderPane;
-import me.belakede.thesis.client.boundary.javafx.util.ControlLoader;
-import me.belakede.thesis.game.Game;
-import me.belakede.thesis.game.equipment.Card;
-import me.belakede.thesis.game.equipment.Figurine;
-
-import java.util.Collection;
+import me.belakede.thesis.client.service.SpringFxmlLoader;
 
 public class GamePane extends BorderPane {
 
-    private final ObjectProperty<Game> game = new SimpleObjectProperty<>();
-    private final ObjectProperty<Figurine> figurine = new SimpleObjectProperty<>();
-    private final ListProperty<Card> cards = new SimpleListProperty<>();
-
-    @FXML
-    private SideBar sideBar;
-    @FXML
-    private BoardPane boardPane;
+    private static final SpringFxmlLoader SPRING_FXML_LOADER = new SpringFxmlLoader();
 
     public GamePane() {
-        load();
-        hookupChangeListeners();
-        transformBoard();
-    }
-
-    public GamePane(Game game, Figurine figurine, Collection<Card> cards) {
-        load();
-        hookupChangeListeners();
-        setFigurine(figurine);
-        setCards(FXCollections.observableArrayList(cards));
-        setGame(game);
-        transformBoard();
-    }
-
-    public Game getGame() {
-        return game.get();
-    }
-
-    public void setGame(Game game) {
-        this.game.set(game);
-    }
-
-    public ObjectProperty<Game> gameProperty() {
-        return game;
-    }
-
-    public Figurine getFigurine() {
-        return figurine.get();
-    }
-
-    public void setFigurine(Figurine figurine) {
-        this.figurine.set(figurine);
-    }
-
-    public ObjectProperty<Figurine> figurineProperty() {
-        return figurine;
-    }
-
-    public ObservableList<Card> getCards() {
-        return cards.get();
-    }
-
-    public void setCards(ObservableList<Card> cards) {
-        this.cards.set(cards);
-    }
-
-    public ListProperty<Card> cardsProperty() {
-        return cards;
-    }
-
-    private void hookupChangeListeners() {
-        sideBar.figurineProperty().bind(figurineProperty());
-        cardsProperty().addListener((observable, oldValue, newValue) -> {
-            if (null != newValue) {
-                sideBar.setCards(cards);
-            }
-        });
-        gameProperty().addListener((observable, oldValue, newValue) -> {
-            if (null != newValue) {
-                boardPane.setBoardType(newValue.getBoard().getBoardType());
-                boardPane.setFigurines(FXCollections.observableMap(newValue.getPositions()));
-                sideBar.bindSize(boardPane.widthProperty());
-            }
-        });
-    }
-
-    private void load() {
-        ControlLoader.load(this);
-    }
-
-    private void transformBoard() {
-        boardPane.getTransforms().addAll(sideBar.getRotates());
+        SPRING_FXML_LOADER.load(this);
     }
 
 }
