1447d819c5f4745cfe208fa34a5aa79aa4fe718d
belakede <belakede@gmail.com>
2016-11-17 22:48:41 +0100

    Change CardPane

--- src/main/java/me/belakede/thesis/client/boundary/javafx/control/CardPane.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/control/CardPane.java
@@ -1,50 +1,14 @@
 package me.belakede.thesis.client.boundary.javafx.control;
 
-import javafx.beans.property.ListProperty;
-import javafx.beans.property.SimpleListProperty;
-import javafx.collections.ObservableList;
 import javafx.scene.layout.HBox;
-import me.belakede.thesis.client.boundary.javafx.util.ControlLoader;
-import me.belakede.thesis.game.equipment.Card;
+import me.belakede.thesis.client.service.SpringFxmlLoader;
 
 public class CardPane extends HBox {
 
-    private final ListProperty<Card> cards = new SimpleListProperty<>();
+    private static final SpringFxmlLoader SPRING_FXML_LOADER = new SpringFxmlLoader();
 
     public CardPane() {
-        load();
-        hookupChangeListeners();
-    }
-
-    public CardPane(ObservableList<Card> cards) {
-        load();
-        hookupChangeListeners();
-        setCards(cards);
-    }
-
-    public ObservableList<Card> getCards() {
-        return cards.get();
-    }
-
-    public void setCards(ObservableList<Card> cards) {
-        this.cards.set(cards);
-    }
-
-    public ListProperty<Card> cardsProperty() {
-        return cards;
-    }
-
-    private void load() {
-        ControlLoader.load(this);
-    }
-
-    private void hookupChangeListeners() {
-        cardsProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue != null) {
-                getChildren().clear();
-                newValue.stream().map(CardBox::new).forEach(card -> getChildren().add(card));
-            }
-        });
+        SPRING_FXML_LOADER.load(this);
     }
 
 }
