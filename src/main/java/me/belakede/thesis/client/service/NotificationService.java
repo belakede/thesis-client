9524c7ea3ed12ffd534719c15b83da8b246cea1d
belakede <belakede@gmail.com>
2016-11-17 02:00:56 +0100

    Add PlayerJoinedNotification list to NotificationService

--- src/main/java/me/belakede/thesis/client/service/NotificationService.java
+++ src/main/java/me/belakede/thesis/client/service/NotificationService.java
@@ -3,17 +3,22 @@ package me.belakede.thesis.client.service;
 import javafx.beans.property.ListProperty;
 import javafx.beans.property.SimpleListProperty;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import me.belakede.thesis.server.game.response.Notification;
+import me.belakede.thesis.server.game.response.PlayerJoinedNotification;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationService {
 
     private final ListProperty<Notification> notifications = new SimpleListProperty<>();
+    private final ListProperty<PlayerJoinedNotification> playerJoinedNotifications = new SimpleListProperty<>();
 
     public NotificationService() {
         setNotifications(FXCollections.observableArrayList());
+        setPlayerJoinedNotifications(FXCollections.observableArrayList());
+        setHookupChangeListeners();
     }
 
     public ObservableList<Notification> getNotifications() {
@@ -28,8 +33,34 @@ public class NotificationService {
         return notifications;
     }
 
+    public ObservableList<PlayerJoinedNotification> getPlayerJoinedNotifications() {
+        return playerJoinedNotifications.get();
+    }
+
+    public void setPlayerJoinedNotifications(ObservableList<PlayerJoinedNotification> playerJoinedNotifications) {
+        this.playerJoinedNotifications.set(playerJoinedNotifications);
+    }
+
+    public ListProperty<PlayerJoinedNotification> playerJoinedNotificationsProperty() {
+        return playerJoinedNotifications;
+    }
+
     public void add(Notification notification) {
         notifications.add(notification);
+    }
+
+    private void setHookupChangeListeners() {
+        notifications.addListener((ListChangeListener.Change<? extends Notification> change) -> {
+            while (change.next()) {
+                if (change.wasAdded()) {
+                    change.getAddedSubList().forEach(notification -> {
+                        if (notification instanceof PlayerJoinedNotification) {
+                            playerJoinedNotifications.add((PlayerJoinedNotification) notification);
+                        }
+                    });
+                }
+            }
+        });
     }
 
 }
