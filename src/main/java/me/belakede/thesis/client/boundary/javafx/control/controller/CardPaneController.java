1a1e90341295603491006029cb248a072a3410a9
belakede <belakede@gmail.com>
2016-11-17 22:48:30 +0100

    Add CardPaneController

---
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/control/controller/CardPaneController.java
@@ -0,0 +1,63 @@
+package me.belakede.thesis.client.boundary.javafx.control.controller;
+
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.HBox;
+import me.belakede.thesis.client.boundary.javafx.control.CardBox;
+import me.belakede.thesis.client.service.GameService;
+import me.belakede.thesis.game.equipment.Card;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+@Controller
+public class CardPaneController implements Initializable {
+
+    private final ListProperty<Card> cards = new SimpleListProperty<>();
+    private final GameService gameService;
+    @FXML
+    private HBox parent;
+
+
+    @Autowired
+    public CardPaneController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        hookupChangeListeners();
+        setupBindings();
+
+    }
+
+    public ObservableList<Card> getCards() {
+        return cards.get();
+    }
+
+    public void setCards(ObservableList<Card> cards) {
+        this.cards.set(cards);
+    }
+
+    public ListProperty<Card> cardsProperty() {
+        return cards;
+    }
+
+    private void hookupChangeListeners() {
+        cardsProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                parent.getChildren().clear();
+                newValue.stream().map(CardBox::new).forEach(card -> parent.getChildren().add(card));
+            }
+        });
+    }
+
+    private void setupBindings() {
+        cardsProperty().bind(gameService.cardsProperty());
+    }
+}
