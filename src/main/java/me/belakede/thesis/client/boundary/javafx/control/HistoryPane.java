4a686340e1807f42d97548898d7ff50e1669093a
Adam Belak <belakede@gmail.com>
2016-11-17 14:56:03 +0100

    Change HistoryPane

--- src/main/java/me/belakede/thesis/client/boundary/javafx/control/HistoryPane.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/control/HistoryPane.java
@@ -1,70 +1,14 @@
 package me.belakede.thesis.client.boundary.javafx.control;
 
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
-import me.belakede.thesis.client.boundary.javafx.util.ControlLoader;
-import me.belakede.thesis.game.equipment.Figurine;
-import org.controlsfx.control.PopOver;
+import me.belakede.thesis.client.service.SpringFxmlLoader;
 
 public class HistoryPane extends StackPane {
 
-    private final ObjectProperty<Figurine> figurine = new SimpleObjectProperty<>();
-    @FXML
-    private Button historyButton;
-    private PopOver popOver;
+    private static final SpringFxmlLoader SPRING_FXML_LOADER = new SpringFxmlLoader();
 
     public HistoryPane() {
-        load();
-        setupPopover();
-        hookupChangeListeners();
-    }
-
-    public HistoryPane(Figurine figurine) {
-        load();
-        setupPopover();
-        hookupChangeListeners();
-        setFigurine(figurine);
-    }
-
-    public Figurine getFigurine() {
-        return figurine.get();
-    }
-
-    public void setFigurine(Figurine figurine) {
-        this.figurine.set(figurine);
-    }
-
-    public ObjectProperty<Figurine> figurineProperty() {
-        return figurine;
-    }
-
-    private void load() {
-        ControlLoader.load(this);
-    }
-
-    private void setupPopover() {
-        popOver = new PopOver(new HistoryBox());
-        popOver.setAnimated(true);
-        popOver.setTitle("History");
-        popOver.setHeaderAlwaysVisible(true);
-        popOver.setDetachable(false);
-        popOver.setDetached(false);
-        popOver.setArrowLocation(PopOver.ArrowLocation.BOTTOM_RIGHT);
-    }
-
-    private void hookupChangeListeners() {
-        figurine.addListener((observable, oldValue, newValue) -> {
-            if (newValue != null) {
-                if (oldValue != null) {
-                    historyButton.getStyleClass().remove(oldValue.name().toLowerCase());
-                }
-                historyButton.getStyleClass().add(newValue.name().toLowerCase());
-            }
-        });
-        historyButton.setOnAction(event -> popOver.show(this));
+        SPRING_FXML_LOADER.load(this);
     }
 
 }
