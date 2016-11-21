3424ef66f577e8eb8b6222b9bc19bbaf568fbc8e
Adam Belak <belakede@gmail.com>
2016-11-21 16:43:34 +0100

    Add AccuseTask and SuspectTask

---
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/task/SuspectTask.java
@@ -0,0 +1,48 @@
+package me.belakede.thesis.client.boundary.javafx.task;
+
+import javafx.concurrent.Task;
+import me.belakede.thesis.client.service.UserService;
+import me.belakede.thesis.game.equipment.Room;
+import me.belakede.thesis.game.equipment.Suspect;
+import me.belakede.thesis.game.equipment.Weapon;
+import me.belakede.thesis.server.game.request.SuspectRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+public class SuspectTask extends Task<Void> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SuspectTask.class);
+    private final UserService userService;
+    private final Suspect suspect;
+    private final Room room;
+    private final Weapon weapon;
+
+    public SuspectTask(UserService userService, Suspect suspect, Room room, Weapon weapon) {
+        this.userService = userService;
+        this.suspect = suspect;
+        this.room = room;
+        this.weapon = weapon;
+    }
+
+    @Override
+    protected Void call() throws Exception {
+        Client client = ClientBuilder.newClient();
+        WebTarget webTarget = client.target(userService.getUrl("/game/suspect"));
+        Response response = webTarget.request().accept(MediaType.APPLICATION_JSON_TYPE)
+                .header("Authorization", "Bearer " + userService.getAccessToken())
+                .post(Entity.json(new SuspectRequest(suspect, room, weapon)));
+        if (response.getStatus() != 200) {
+            LOGGER.warn("HTTP error code : {}", response.getStatus());
+            LOGGER.warn("{}", response.toString());
+            throw new RuntimeException("Suspicion failed. Maybe it's not your turn.");
+        }
+        return null;
+    }
+}
