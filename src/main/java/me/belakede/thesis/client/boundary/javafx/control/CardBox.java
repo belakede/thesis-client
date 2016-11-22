3b5c065a8ed042429267df13a5c0bbe5fc2ce58b
belakede <belakede@gmail.com>
2016-11-22 22:00:06 +0100

    Redesign CardBox

--- src/main/java/me/belakede/thesis/client/boundary/javafx/control/CardBox.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/control/CardBox.java
@@ -1,54 +1,17 @@
 package me.belakede.thesis.client.boundary.javafx.control;
 
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.fxml.FXML;
-import javafx.scene.layout.StackPane;
-import javafx.scene.text.Text;
-import me.belakede.thesis.client.boundary.javafx.util.ControlLoader;
+import javafx.scene.layout.AnchorPane;
+import me.belakede.thesis.client.boundary.javafx.control.controller.CardBoxController;
+import me.belakede.thesis.client.service.SpringFxmlLoader;
 import me.belakede.thesis.game.equipment.Card;
 
-public class CardBox extends StackPane {
+public class CardBox extends AnchorPane {
 
-    private final ObjectProperty<Card> card = new SimpleObjectProperty<>();
-
-    @FXML
-    private Text label;
-
-    public CardBox() {
-        load();
-        hookupChangeListeners();
-    }
+    private static final SpringFxmlLoader SPRING_FXML_LOADER = new SpringFxmlLoader();
 
     public CardBox(Card card) {
-        load();
-        hookupChangeListeners();
-        setCard(card);
-    }
-
-    public Card getCard() {
-        return card.get();
-    }
-
-    public void setCard(Card card) {
-        this.card.set(card);
-    }
-
-    public ObjectProperty<Card> cardProperty() {
-        return card;
-    }
-
-    private void load() {
-        ControlLoader.load(this);
-    }
-
-    private void hookupChangeListeners() {
-        cardProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue != null) {
-                String cardTitle = newValue.name().toLowerCase().replace('_', ' ');
-                label.setText(cardTitle);
-            }
-        });
+        CardBoxController controller = SPRING_FXML_LOADER.load(this);
+        controller.setCard(card);
     }
 
 }
---
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/control/controller/CardBoxController.java
@@ -0,0 +1,75 @@
+package me.belakede.thesis.client.boundary.javafx.control.controller;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+import me.belakede.thesis.client.service.UserService;
+import me.belakede.thesis.game.equipment.Card;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import java.net.URL;
+import java.util.Collections;
+import java.util.ResourceBundle;
+
+@Controller
+@Scope("prototype")
+public class CardBoxController implements Initializable {
+
+    private final ObjectProperty<Card> card = new SimpleObjectProperty<>();
+    private final UserService userService;
+
+    @FXML
+    private AnchorPane parent;
+    @FXML
+    private Text label;
+    @FXML
+    private Text labelUpsideDown;
+
+    public CardBoxController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        setupBindings();
+        hookupChangeListeners();
+    }
+
+    public Card getCard() {
+        return card.get();
+    }
+
+    public void setCard(Card card) {
+        this.card.set(card);
+    }
+
+    public ObjectProperty<Card> cardProperty() {
+        return card;
+    }
+
+    private void setupBindings() {
+        labelUpsideDown.textProperty().bind(label.textProperty());
+    }
+
+    private void hookupChangeListeners() {
+        cardProperty().addListener((observable, oldValue, newValue) -> {
+            if (oldValue != null) {
+                parent.getStyleClass().removeAll(Collections.singleton(createStyleClassFromCard(oldValue)));
+            }
+            parent.getStyleClass().add(createStyleClassFromCard(newValue));
+            label.setText(createTextFromCard(newValue));
+        });
+    }
+
+    private String createTextFromCard(Card card) {
+        return card.name().toLowerCase().replace('_', ' ');
+    }
+
+    private String createStyleClassFromCard(Card card) {
+        return card.name().toLowerCase().replace("_", "-");
+    }
+}
