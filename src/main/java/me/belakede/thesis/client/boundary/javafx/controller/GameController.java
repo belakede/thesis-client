a51a7d644cca58f25dadc873f56fac0d48370dd5
Adam Belak <belakede@gmail.com>
2016-11-22 15:09:56 +0100

    Improve GameController to display endPane when somebody won the game

--- src/main/java/me/belakede/thesis/client/boundary/javafx/controller/GameController.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/controller/GameController.java
@@ -1,14 +1,20 @@
 package me.belakede.thesis.client.boundary.javafx.controller;
 
+import javafx.application.Platform;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.collections.ListChangeListener;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import me.belakede.thesis.client.boundary.javafx.control.ChatPane;
 import me.belakede.thesis.client.boundary.javafx.control.GamePane;
 import me.belakede.thesis.client.boundary.javafx.control.HistoryPane;
 import me.belakede.thesis.client.boundary.javafx.control.NotePane;
+import me.belakede.thesis.client.service.GameService;
 import me.belakede.thesis.client.service.NotificationService;
 import me.belakede.thesis.client.service.SnapshotService;
 import me.belakede.thesis.server.game.response.Notification;
@@ -16,14 +22,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import java.net.URL;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.ResourceBundle;
 
 @Controller
 public class GameController implements Initializable {
 
-    private final NotificationService notificationService;
+    private final GameService gameService;
     private final SnapshotService snapshotService;
-
+    private final LoungeController loungeController;
+    private final NotificationService notificationService;
+    @FXML
+    public StackPane endPane;
+    @FXML
+    public HBox playerContainer;
     @FXML
     private AnchorPane parent;
     @FXML
@@ -38,9 +51,11 @@ public class GameController implements Initializable {
     private StackPane sorryPane;
 
     @Autowired
-    public GameController(NotificationService notificationService, SnapshotService snapshotService) {
-        this.notificationService = notificationService;
+    public GameController(GameService gameService, SnapshotService snapshotService, LoungeController loungeController, NotificationService notificationService) {
+        this.gameService = gameService;
         this.snapshotService = snapshotService;
+        this.loungeController = loungeController;
+        this.notificationService = notificationService;
     }
 
     public void initialize(URL location, ResourceBundle resources) {
@@ -56,12 +71,35 @@ public class GameController implements Initializable {
             }
         });
         notificationService.gamePausedNotificationProperty().addListener((observable, oldValue, newValue) -> {
-            gamePane.setDisable(true);
-            notePane.setDisable(true);
-            gamePane.setVisible(false);
-            notePane.setVisible(false);
-            sorryPane.setVisible(true);
+            disablePanes();
+            if (notificationService.getGameEndedNotification() == null) {
+                sorryPane.setVisible(true);
+            }
         });
+        notificationService.gameEndedNotificationProperty().addListener((observable, oldValue, newValue) -> {
+            disablePanes();
+            final StringProperty winnerClass = new SimpleStringProperty();
+            String winner = newValue.getWinner();
+            if (winner != null) {
+                winnerClass.setValue(gameService.getReversedPlayers().get(winner).name().toLowerCase());
+            }
+            Collection<VBox> playerBoxes = loungeController.getPlayerBoxes().values();
+            playerBoxes.stream()
+                    .filter(playerBox -> winner == null || !playerBox.getStyleClass().contains(winnerClass.getValue()))
+                    .forEach(playerBox -> playerBox.getStyleClass().removeAll(Collections.singleton("online")));
+            Platform.runLater(() -> {
+                playerContainer.getChildren().clear();
+                playerContainer.getChildren().addAll(playerBoxes);
+                endPane.setVisible(true);
+            });
+        });
+    }
+
+    private void disablePanes() {
+        gamePane.setDisable(true);
+        notePane.setDisable(true);
+        gamePane.setVisible(false);
+        notePane.setVisible(false);
     }
 
 }
