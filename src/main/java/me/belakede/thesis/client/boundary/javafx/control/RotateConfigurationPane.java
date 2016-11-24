52f36f8b63d6ddf0f129859373cb4b02f31d03a9
belakede <belakede@gmail.com>
2016-11-24 21:41:23 +0100

    Add RotateConfigurationPaneController and get texts from resource bundle

--- src/main/java/me/belakede/thesis/client/boundary/javafx/control/RotateConfigurationPane.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/control/RotateConfigurationPane.java
@@ -1,78 +1,27 @@
 package me.belakede.thesis.client.boundary.javafx.control;
 
 import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.SimpleDoubleProperty;
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
-import javafx.geometry.Point3D;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
 import javafx.scene.layout.VBox;
 import javafx.scene.transform.Rotate;
-import me.belakede.thesis.client.boundary.javafx.util.ControlLoader;
+import me.belakede.thesis.client.boundary.javafx.control.controller.RotateConfigurationPaneController;
+import me.belakede.thesis.client.service.SpringFxmlLoader;
 
 public class RotateConfigurationPane extends VBox {
 
-    private final ObservableList<Rotate> rotates = FXCollections.observableArrayList();
-    private final DoubleProperty size = new SimpleDoubleProperty();
-    private final DoubleProperty pivot = new SimpleDoubleProperty();
-
-    @FXML
-    private Slider roll;
-    @FXML
-    private Label rollValue;
+    private static final SpringFxmlLoader SPRING_FXML_LOADER = new SpringFxmlLoader();
+    private final RotateConfigurationPaneController controller;
 
     public RotateConfigurationPane() {
-        load();
-        hookupChangeListeners();
-    }
-
-    public RotateConfigurationPane(double size) {
-        load();
-        hookupChangeListeners();
-        setSize(size);
-    }
-
-    public ObservableList<Rotate> getRotates() {
-        if (rotates.isEmpty()) {
-            uploadRotates();
-        }
-        return rotates;
-    }
-
-    public double getSize() {
-        return size.get();
-    }
-
-    public void setSize(double size) {
-        this.size.set(size);
+        controller = SPRING_FXML_LOADER.load(this);
     }
 
     public DoubleProperty sizeProperty() {
-        return size;
+        return controller.sizeProperty();
     }
 
-    private void load() {
-        ControlLoader.load(this);
+    public ObservableList<Rotate> getRotates() {
+        return controller.getRotates();
     }
 
-    private void uploadRotates() {
-        rotates.addAll(createRotation(roll, Rotate.Z_AXIS));
-    }
-
-    private Rotate createRotation(Slider slider, Point3D point3D) {
-        Rotate rotate = new Rotate(0, 0, 0, 0, point3D);
-        rotate.pivotXProperty().bind(pivot);
-        rotate.pivotYProperty().bind(pivot);
-        rotate.pivotZProperty().bind(pivot);
-        rotate.angleProperty().bind(slider.valueProperty());
-        return rotate;
-    }
-
-    private void hookupChangeListeners() {
-        pivot.bind(sizeProperty().divide(2.0));
-        roll.valueProperty().addListener((observable, oldValue, newValue) -> roll.setValue(newValue.intValue()));
-        rollValue.textProperty().bind(roll.valueProperty().asString("Roll with %.0f deg"));
-    }
 }
---
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/control/controller/RotateConfigurationPaneController.java
@@ -0,0 +1,78 @@
+package me.belakede.thesis.client.boundary.javafx.control.controller;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Point3D;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.transform.Rotate;
+import me.belakede.thesis.client.boundary.javafx.util.ControlLoader;
+import org.springframework.stereotype.Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+@Controller
+public class RotateConfigurationPaneController implements Initializable {
+
+    private final ObservableList<Rotate> rotates = FXCollections.observableArrayList();
+    private final DoubleProperty size = new SimpleDoubleProperty();
+    private final DoubleProperty pivot = new SimpleDoubleProperty();
+
+    @FXML
+    private Slider roll;
+    @FXML
+    private Label rollValue;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        hookupChangeListeners();
+    }
+
+    public ObservableList<Rotate> getRotates() {
+        if (rotates.isEmpty()) {
+            uploadRotates();
+        }
+        return rotates;
+    }
+
+    public double getSize() {
+        return size.get();
+    }
+
+    public void setSize(double size) {
+        this.size.set(size);
+    }
+
+    public DoubleProperty sizeProperty() {
+        return size;
+    }
+
+    private void load() {
+        ControlLoader.load(this);
+    }
+
+    private void uploadRotates() {
+        rotates.addAll(createRotation(roll, Rotate.Z_AXIS));
+    }
+
+    private Rotate createRotation(Slider slider, Point3D point3D) {
+        Rotate rotate = new Rotate(0, 0, 0, 0, point3D);
+        rotate.pivotXProperty().bind(pivot);
+        rotate.pivotYProperty().bind(pivot);
+        rotate.pivotZProperty().bind(pivot);
+        rotate.angleProperty().bind(slider.valueProperty());
+        return rotate;
+    }
+
+    private void hookupChangeListeners() {
+        pivot.bind(sizeProperty().divide(2.0));
+        roll.valueProperty().addListener((observable, oldValue, newValue) -> roll.setValue(newValue.intValue()));
+        rollValue.textProperty().bind(roll.valueProperty().asString("Roll with %.0f deg"));
+    }
+
+}
