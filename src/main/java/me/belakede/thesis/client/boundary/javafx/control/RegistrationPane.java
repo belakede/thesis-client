bac858d759d72540511d2cb36b310d0a7f8b234e
belakede <belakede@gmail.com>
2016-11-24 21:13:42 +0100

    Add RegistrationPaneController and get texts from resource bundle

--- src/main/java/me/belakede/thesis/client/boundary/javafx/control/RegistrationPane.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/control/RegistrationPane.java
@@ -1,70 +1,15 @@
 package me.belakede.thesis.client.boundary.javafx.control;
 
-import javafx.concurrent.Task;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
-import me.belakede.thesis.client.boundary.javafx.task.RegistrationTask;
-import org.controlsfx.control.NotificationPane;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import static me.belakede.thesis.client.boundary.javafx.util.ControlLoader.load;
+import me.belakede.thesis.client.service.SpringFxmlLoader;
 
 public class RegistrationPane extends VBox {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(RegistrationPane.class);
+    private static final SpringFxmlLoader SPRING_FXML_LOADER = new SpringFxmlLoader();
 
-    @FXML
-    private NotificationPane notificationPane;
-    @FXML
-    private TextField serverAddress;
-    @FXML
-    private TextField username;
-    @FXML
-    private PasswordField password;
-    @FXML
-    private PasswordField passwordAgain;
-    @FXML
-    private Button submit;
 
     public RegistrationPane() {
-        load(this);
-        setupNotificationPane();
-        setupActionEvents();
+        SPRING_FXML_LOADER.load(this);
     }
 
-    private void setupNotificationPane() {
-        notificationPane.setShowFromTop(true);
-        notificationPane.prefHeightProperty().bind(heightProperty());
-    }
-
-    private void setupActionEvents() {
-        submit.setOnAction(event -> {
-            if (password.getText().isEmpty() || password.getText().length() < 5 || !password.getText().equals(passwordAgain.getText())) {
-                LOGGER.info("Missing or wrong password!");
-                notificationPane.setText("Missing or wrong password!");
-                notificationPane.show();
-            } else {
-                Task task = new RegistrationTask(serverAddress.getText(), username.getText(), password.getText());
-                task.setOnFailed(failedEvent -> {
-                    LOGGER.info("Registration failed!");
-                    notificationPane.setText("Registration failed!");
-                    notificationPane.show();
-                });
-                task.setOnSucceeded(succeededEvent -> {
-                    LOGGER.info("Registration succeeded!");
-                    submit.setDisable(true);
-                    notificationPane.setText("Registration succeeded!");
-                    notificationPane.getContent().setDisable(true);
-                    notificationPane.show();
-                });
-                Thread thread = new Thread(task);
-                thread.setDaemon(true);
-                thread.start();
-            }
-        });
-    }
 }
---
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/control/controller/RegistrationPaneController.java
@@ -0,0 +1,76 @@
+package me.belakede.thesis.client.boundary.javafx.control.controller;
+
+import javafx.concurrent.Task;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import me.belakede.thesis.client.boundary.javafx.task.RegistrationTask;
+import org.controlsfx.control.NotificationPane;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+@Controller
+public class RegistrationPaneController implements Initializable {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RegistrationPaneController.class);
+
+    @FXML
+    private VBox parent;
+    @FXML
+    private NotificationPane notificationPane;
+    @FXML
+    private TextField serverAddress;
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private PasswordField passwordAgain;
+    @FXML
+    private Button submit;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        setupNotificationPane();
+        setupActionEvents();
+    }
+
+    private void setupNotificationPane() {
+        notificationPane.setShowFromTop(true);
+        notificationPane.prefHeightProperty().bind(parent.heightProperty());
+    }
+
+    private void setupActionEvents() {
+        submit.setOnAction(event -> {
+            if (password.getText().isEmpty() || password.getText().length() < 5 || !password.getText().equals(passwordAgain.getText())) {
+                LOGGER.info("Missing or wrong password!");
+                notificationPane.setText("Missing or wrong password!");
+                notificationPane.show();
+            } else {
+                Task task = new RegistrationTask(serverAddress.getText(), username.getText(), password.getText());
+                task.setOnFailed(failedEvent -> {
+                    LOGGER.info("Registration failed!");
+                    notificationPane.setText("Registration failed!");
+                    notificationPane.show();
+                });
+                task.setOnSucceeded(succeededEvent -> {
+                    LOGGER.info("Registration succeeded!");
+                    submit.setDisable(true);
+                    notificationPane.setText("Registration succeeded!");
+                    notificationPane.getContent().setDisable(true);
+                    notificationPane.show();
+                });
+                Thread thread = new Thread(task);
+                thread.setDaemon(true);
+                thread.start();
+            }
+        });
+    }
+}
