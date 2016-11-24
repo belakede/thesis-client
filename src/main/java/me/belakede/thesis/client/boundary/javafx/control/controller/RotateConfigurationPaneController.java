346d04a212a0444478575fb1fbafe179d4247eb7
belakede <belakede@gmail.com>
2016-11-24 23:41:23 +0100

    Add board auto rotation

--- src/main/java/me/belakede/thesis/client/boundary/javafx/control/controller/RotateConfigurationPaneController.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/control/controller/RotateConfigurationPaneController.java
@@ -1,5 +1,6 @@
 package me.belakede.thesis.client.boundary.javafx.control.controller;
 
+import javafx.application.Platform;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.collections.FXCollections;
@@ -11,6 +12,9 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.transform.Rotate;
 import me.belakede.thesis.client.boundary.javafx.util.ControlLoader;
+import me.belakede.thesis.client.service.PlayerService;
+import me.belakede.thesis.game.field.Field;
+import org.controlsfx.control.ToggleSwitch;
 import org.springframework.stereotype.Controller;
 
 import java.net.URL;
@@ -22,12 +26,19 @@ public class RotateConfigurationPaneController implements Initializable {
     private final ObservableList<Rotate> rotates = FXCollections.observableArrayList();
     private final DoubleProperty size = new SimpleDoubleProperty();
     private final DoubleProperty pivot = new SimpleDoubleProperty();
+    private final PlayerService playerService;
     private ResourceBundle resources;
 
     @FXML
     private Slider roll;
     @FXML
     private Label rollValue;
+    @FXML
+    private ToggleSwitch autoRotate;
+
+    public RotateConfigurationPaneController(PlayerService playerService) {
+        this.playerService = playerService;
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -72,9 +83,23 @@ public class RotateConfigurationPaneController implements Initializable {
     }
 
     private void hookupChangeListeners() {
+        playerService.fieldProperty().addListener((observable, oldValue, newValue) -> autoRotation(newValue));
         pivot.bind(sizeProperty().divide(2.0));
         roll.valueProperty().addListener((observable, oldValue, newValue) -> roll.setValue(newValue.intValue()));
         rollValue.textProperty().bind(roll.valueProperty().asString(getResources().getString("Roll with %.0f deg")));
+        autoRotation(playerService.getField());
+    }
+
+    private void autoRotation(Field field) {
+        if (autoRotate.isSelected()) {
+            Platform.runLater(() -> {
+                if (field.getRow() < 14) {
+                    roll.setValue(field.getColumn() < 14 ? -90 : -180);
+                } else {
+                    roll.setValue(field.getColumn() < 14 ? 0 : 90);
+                }
+            });
+        }
     }
 
     public ResourceBundle getResources() {
