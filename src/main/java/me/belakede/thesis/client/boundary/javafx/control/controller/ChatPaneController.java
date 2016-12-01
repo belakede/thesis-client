9bc4443faed68ebc5972a26bc98625e0a689d287
belakede <belakede@gmail.com>
2016-12-01 18:54:29 +0100

    Add notification circle to chat pane

--- src/main/java/me/belakede/thesis/client/boundary/javafx/control/controller/ChatPaneController.java
+++ src/main/java/me/belakede/thesis/client/boundary/javafx/control/controller/ChatPaneController.java
@@ -2,11 +2,15 @@ package me.belakede.thesis.client.boundary.javafx.control.controller;
 
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.ListChangeListener.Change;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Circle;
 import me.belakede.thesis.client.boundary.javafx.control.ChatBox;
+import me.belakede.thesis.client.boundary.javafx.model.UserMessage;
+import me.belakede.thesis.client.service.MessageService;
 import me.belakede.thesis.client.service.PlayerService;
 import me.belakede.thesis.game.equipment.Figurine;
 import org.controlsfx.control.PopOver;
@@ -21,17 +25,21 @@ public class ChatPaneController implements Initializable {
 
     private final ObjectProperty<Figurine> figurine = new SimpleObjectProperty<>();
     private final PlayerService playerService;
+    private final MessageService messageService;
     private ResourceBundle resources;
 
     @FXML
     private StackPane parent;
     @FXML
     private Button chatButton;
+    @FXML
+    private Circle notification;
     private PopOver popOver;
 
     @Autowired
-    public ChatPaneController(PlayerService playerService) {
+    public ChatPaneController(PlayerService playerService, MessageService messageService) {
         this.playerService = playerService;
+        this.messageService = messageService;
     }
 
     @Override
@@ -54,10 +62,22 @@ public class ChatPaneController implements Initializable {
         figurine.addListener((observable, oldValue, newValue) -> {
             if (oldValue != null) {
                 chatButton.getStyleClass().remove(oldValue.name().toLowerCase());
+                notification.getStyleClass().remove(oldValue.name().toLowerCase());
             }
             chatButton.getStyleClass().add(newValue.name().toLowerCase());
+            notification.getStyleClass().add(newValue.name().toLowerCase());
         });
-        chatButton.setOnAction(event -> popOver.show(parent));
+        messageService.messagesProperty().addListener((Change<? extends UserMessage> change) -> {
+            while (change.next()) {
+                if (change.wasAdded()) {
+                    notification.setVisible(true);
+                }
+            }
+        });
+        chatButton.setOnAction(event -> {
+            popOver.show(parent);
+            notification.setVisible(false);
+        });
     }
 
     private void setupBindings() {
